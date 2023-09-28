849dabc7efceb790e0a38042f5c5bcc90048d076
sosiska52 <nikuta.makarevich@gmail.com>
2023-09-28 10:19:38 +0300

    1

---
+++ trunk/ii02313/task_01/src/otis_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double a = 0.6, b = 0.5, c = 0.65, d = 1.1, u = 4.1;
+
+double func2(double prev){
+	double next = a * prev + b * u;
+	return next;
+}
+
+double func3(double prev) {
+	double next = a * prev - prev * prev * b + u * c + d * sin(u);
+	return next;
+}
+
+int main()
+{
+	double y1 = 0.6, prev1 = y1, prev2 = y1;
+	int t;
+	cout << "Enter t: ";
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cout << i << ";" << func2(prev1) << ";" << func3(prev2)<< endl;
+		prev1 = func2(prev1);
+		prev2 = func3(prev2);
+	}
+	return 0;
+}
