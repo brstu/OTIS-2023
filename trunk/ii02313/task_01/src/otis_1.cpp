46e9fad66ce362e8e36b598f172d9162971e021e
sosiska52 <nikuta.makarevich@gmail.com>
2023-09-28 10:43:46 +0300

    1

--- trunk/ii02313/task_01/src/otis_1.cpp
+++ trunk/ii02313/task_01/src/otis_1.cpp
@@ -3,17 +3,23 @@
 
 using namespace std;
 
-double a = 0.6, b = 0.5, c = 0.65, d = 1.1, u = 4.1;
+class Function {
+private:
+	double a = 0.6, b = 0.5, c = 0.65, d = 1.1, u = 4.1;
+public:
+	double func2(double prev) {
+		double next = a * prev + b * u;
+		return next;
+	}
 
-double func2(double prev){
-	double next = a * prev + b * u;
-	return next;
-}
+	double func3(double prev) {
+		double next = a * prev - prev * prev * b + u * c + d * sin(u);
+		return next;
+	}
+};
 
-double func3(double prev) {
-	double next = a * prev - prev * prev * b + u * c + d * sin(u);
-	return next;
-}
+
+
 
 int main()
 {
@@ -21,11 +27,11 @@ int main()
 	int t;
 	cout << "Enter t: ";
 	cin >> t;
-
+	Function f;
 	for (int i = 0; i < t; i++) {
-		cout << i << ";" << func2(prev1) << ";" << func3(prev2)<< endl;
-		prev1 = func2(prev1);
-		prev2 = func3(prev2);
+		cout << i << ";" << f.func2(prev1) << ";" << f.func3(prev2)<< endl;
+		prev1 = f.func2(prev1);
+		prev2 = f.func3(prev2);
 	}
 	return 0;
 }
