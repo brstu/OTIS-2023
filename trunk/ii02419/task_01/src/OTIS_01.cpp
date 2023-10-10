293508ea9f2176dc4a51d73ac3ce7e6d0dfdd269
Sukharevich Dmitry <defgolem@gmail.com>
2023-10-10 15:54:06 +0300

    test

---
+++ trunk/ii02419/task_01/src/OTIS_01.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+class Iche {
+public:
+	double a = 0.5, b = 0.7, c = 1.25, d = 0.1, u = 1.5;
+	int t = 50;
+	std::vector<double>y{ 1.1 };
+	std::vector<double>yy{ 1.1 };
+	void linear_f(){
+		double y_1 = a * y.back() + b * u;
+		y.push_back(y_1);
+	}
+	void non_linear_f(){
+		double y_1 = a * yy.back() - b * pow(yy.back(), 2) + c * u + d * sin(u);
+		yy.push_back(y_1);
+	}
+};
+
+void main() {
+	Iche linear;
+	for (int i = 0; i < linear.t; i++) {
+		linear.linear_f();
+	}
+
+	for (int i = 0; i < linear.t; i++) {
+		linear.non_linear_f();
+	}
+
+	for (int i = 0; i < linear.y.size(); i++) {
+		std::cout << linear.y[i] << " ";
+		std::cout << linear.yy[i] << std::endl;
+	}
+}
