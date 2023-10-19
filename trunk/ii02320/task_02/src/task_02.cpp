8d78089bd8e59f53e97522f80d7bf6784c95bdb8
Gomziakoff <lesha.romanyik@gmail.com>
2023-10-19 10:57:32 +0300

    Update task_02.cpp

--- trunk/ii02320/task_02/src/task_02.cpp
+++ trunk/ii02320/task_02/src/task_02.cpp
@@ -5,46 +5,54 @@
 
 using namespace std;
 
-const double K = 0.0001, T = 100, T_D = 100, T_0 = 1;
-const double q_0 = K * (1 + T_D / T_0);
-const double q_1 = -K * (1 + 2 * T_D / T_0 - T_0 / T);
-const double q_2 = K * T_D / T_0;
-
-double q[3] = { q_0, q_1, q_2 };
-double e[3] = { 0, 0, 0 };
-vector<double> y = { 0, 0, 0 };
-double u[2] = { 1, 1 };
-
-double sum() {
-    double sum = 0;
-    for (int i = 0; i < 3; i++) {
-        sum += q[i] * e[i];
+class func {
+private:
+    double K = 0.0001, T = 100, T_D = 100, T_0 = 1;
+    double q_0 = K * (1 + T_D / T_0);
+    double q_1 = -K * (1 + 2 * T_D / T_0 - T_0 / T);
+    double q_2 = K * T_D / T_0;
+    double q[3] = { q_0, q_1, q_2 };
+    double e[3] = { 0, 0, 0 };
+    vector<double> y = { 0, 0, 0 };
+    double u[2] = { 1, 1 };
+public:
+    double sum() {
+        double sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += q[i] * e[i];
+        }
+        return sum;
     }
-    return sum;
-}
 
-void nonlin(int time, double setpoint, double a = 0.5, double b = 0.3, double c = 0.9, double d = 0.7) {
-    for (int i = 0; i < time; i++) {
-        e[0] = setpoint - y[y.size() - 1];
-        e[1] = setpoint - y[y.size() - 2];
-        e[2] = setpoint - y[y.size() - 3];
-        u[0] = u[1] + sum();
-        y.push_back(a * y[y.size() - 1] - b * y[y.size() - 2] * y[y.size() - 2] + c * u[0] + d * sin(u[1]));
-        u[1] = u[0];
+    void nonlin(int time, double setpoint, double a = 0.5, double b = 0.3, double c = 0.9, double d = 0.7) {
+        for (int i = 0; i < time; i++) {
+            e[0] = setpoint - y[y.size() - 1];
+            e[1] = setpoint - y[y.size() - 2];
+            e[2] = setpoint - y[y.size() - 3];
+            u[0] = u[1] + sum();
+            y.push_back(a * y[y.size() - 1] - b * y[y.size() - 2] * y[y.size() - 2] + c * u[0] + d * sin(u[1]));
+            u[1] = u[0];
+        }
     }
-}
+
+    vector<double> getY() {
+        return y;
+    }
+};
 
 int main() {
     double setpoint;
     ofstream out("output.txt");
+    func f;
 
     if (out.is_open()) {
         cout << "Enter the setpoint: ";
         cin >> setpoint;
-        nonlin(100, setpoint);
+        f.nonlin(100, setpoint);
+        vector<double> y_values = f.getY();
 
-        for (int i = 0; i < y.size(); i++) {
-            double scaledValue = y[i] * setpoint / y[y.size() - 1];
+        for (int i = 0; i < y_values.size(); i++) {
+            double scaledValue = y_values[i] * setpoint / y_values[y_values.size() - 1];
             cout << i << " " << scaledValue << endl;
             out << i << " " << scaledValue << endl;
         }
