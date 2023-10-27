91943c561effb858375d0c977db1d6951612881b
Медведь Павел <118657761+Dizzers@users.noreply.github.com>
2023-10-27 17:41:56 +0300

    Update lan1.cpp

--- trunk/ii02316/task_01/src/lan1.cpp
+++ trunk/ii02316/task_01/src/lan1.cpp
@@ -4,47 +4,56 @@
 
 using namespace std;
 
-void linear(double a, double b, double y, double u, double i, double t) {
-    ofstream file("lin.txt");
-    for (int j = i; j <= t; j++) {
-        file << j << " " << y << endl;
-        cout << y << endl;
-        y = a * y + b * u;
+class Function {
+    private:
+        double i = 1; //start time
+        double y = 0; //input temperature
+        double y1 = 0;
+        double u = 1; //input warm
+        double t = 100; //end time
+        const double a = 0.5;
+        const double b = 0.5;
+        const double c = 0.5;
+        const double d = 0.5; //some constants
+public:
+    void linear() {
+        ofstream file("lin.txt");
+        for (double j = i; j <= t; j++) {
+            file << j << " " << y << endl;
+            cout << y << endl;
+            y = a * y + b * u;
+        }
+        cout << "end linear\n";
     }
-    cout << "end linear\n";
-}
 
-void nonlinear(double a, double b, double c, double d, double y, double y1, double u, double i, double t) {
-    ofstream file("nonlin.txt");
-    for (int j = i; j <= t; j++) {
-        file << j << " " << y << endl;
-        cout << y << endl;
-        if (j == 1) {
-            y = a * y - b * y1 * y1 + c * 0 + d * sin(0);
+    void nonlinear() {
+        ofstream file("nonlin.txt");
+        for (double j = i; j <= t; j++) {
+            file << j << " " << y << endl;
+            cout << y << endl;
+            if (j == 1) {
+                y = a * y - b * y1 * y1 + c * 0 + d * sin(0);
+            }
+            else {
+                y = a * y - b * y1 * y1 + c * u + d * sin(u);
+            }
+            y1 = y;
         }
-        else {
-            y = a * y - b * y1 * y1 + c * u + d * sin(u);
-        }
-        y1 = y;
+        cout << "end nonlinear\n";
     }
-    cout << "end nonlinear\n";
-}
+};
+
+
 
 int main() {
     ofstream file1("lin.txt");
     ofstream file2("nonlin.txt");
     file1.clear();
     file2.clear();
-    double i = 1; //start time
-    double y = 0; //input temperature
-    double u = 1; //input warm
-    double t = 100; //end time
-    const double a = 0.5;
-    const double b = 0.5;
-    const double c = 0.5;
-    const double d = 0.5; //some constants
+    
+    Function func;
     cout << "linear model: \n";
-    linear(a, b, y, u, i, t);
+    func.linear();
     cout << "nonlinear model: \n";
-    nonlinear(a, b, c, d, y, y, u, i, t);
+    func.nonlinear();
 }
