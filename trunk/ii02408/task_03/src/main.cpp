4204bde86581111f3afcf7e8c1fd2b4b893f5f55
alkurash <sasha_kurash@inbox.ru>
2023-10-27 13:52:21 +0300

    ff

--- trunk/ii02408/task_03/src/main.cpp
+++ trunk/ii02408/task_03/src/main.cpp
@@ -1,304 +1,167 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <unordered_map>
-#include <fstream>
-#include <sstream>
-#include <queue>
-#include <algorithm>
+#include <map>
+#include <SFML/Graphics.hpp>
 
-struct Node {
-    std::string name;
-    std::string content;
-    // Дополнительные свойства узла
-};
+sf::RenderWindow window;
+sf::Font font;
+sf::Text mainLabel;
+sf::VertexArray vertices(sf::Points);
+std::map<int, sf::Vector2f> vertexCoords;
+std::vector<sf::CircleShape> ovals;
+std::map<int, std::string> cordText;
+int idText = 0;
+int i = 0;
+int edgeCount = 0;
+std::vector<int> edges;
+std::map<int, std::string> cordEdge;
 
-struct Edge {
-    Node* source;
-    Node* target;
-    bool directed;
-    std::string color;
-    // Дополнительные свойства дуги
-};
-
-class Graph {
-private:
-    std::string name;
-    std::vector<Node*> nodes;
-    std::vector<Edge*> edges;
-    std::unordered_map<std::string, Node*> nodeLookup;
-
-public:
-    Graph(std::string name) : name(name) {}
-
-    void addNode(std::string name) {
-        Node* node = new Node{name};
-        nodes.push_back(node);
-        nodeLookup[name] = node;
-    }
-
-    void addEdge(std::string sourceName, std::string targetName, bool directed = false) {
-        Node* source = nodeLookup[sourceName];
-        Node* target = nodeLookup[targetName];
-        Edge* edge = new Edge{source, target, directed};
-        edges.push_back(edge);
-    }
-
-    std::vector<Node*> getNeighbors(Node* node) {
-        std::vector<Node*> neighbors;
-        for (Edge* edge : edges) {
-            if (edge->source == node) {
-                neighbors.push_back(edge->target);
-            } else if (!edge->directed && edge->target == node) {
-                neighbors.push_back(edge->source);
-            }
+void drawVertex() {
+    mainLabel.setString("Вы выбрали рисование вершин, нажмите на свободное место для рисования вершины");
+    window.unbind(sf::Mouse::Left);
+    window.bind(sf::Mouse::Left, [](sf::Event event) {
+        if (event.type == sf::Event::MouseButtonPressed) {
+            float x = event.mouseButton.x;
+            float y = event.mouseButton.y;
+            sf::CircleShape vertex(15);
+            vertex.setFillColor(sf::Color::Red);
+            vertex.setPosition(x - 15, y - 15);
+            vertices.append(vertex);
+            vertexCoords[i] = sf::Vector2f(x, y);
+            i++;
         }
-        return neighbors;
-    }
+    });
+}
 
-    bool isConnected() {
-        if (nodes.empty()) {
-            return true; // Пустой граф считается связанным
+void drawEdge() {
+    mainLabel.setString("Вы выбрали рисование ребер, нажмите на вершину для рисования ребра");
+    window.unbind(sf::Mouse::Left);
+    window.bind(sf::Mouse::Left, [](sf::Event event) {
+        if (event.type == sf::Event::MouseButtonPressed) {
+            float x = event.mouseButton.x;
+            float y = event.mouseButton.y;
+            // TODO: Implement drawing edges
         }
+    });
+}
 
-        std::unordered_map<Node*, bool> visited;
-        dfs(nodes[0], visited);
-
-        for (Node* node : nodes) {
-            if (visited.find(node) == visited.end()) {
-                return false;
-            }
+void deleteVertex() {
+    mainLabel.setString("Вы выбрали удаление вершины или ребра, нажмите на вершину или ребро для удаления");
+    window.unbind(sf::Mouse::Left);
+    window.bind(sf::Mouse::Left, [](sf::Event event) {
+        if (event.type == sf::Event::MouseButtonPressed) {
+            float x = event.mouseButton.x;
+            float y = event.mouseButton.y;
+            // TODO: Implement vertex or edge deletion
         }
-        return true;
-    }
+    });
+}
 
-    void dfs(Node* node, std::unordered_map<Node*, bool>& visited) {
-        visited[node] = true;
-        std::vector<Node*> neighbors = getNeighbors(node);
-        for (Node* neighbor : neighbors) {
-            if (visited.find(neighbor) == visited.end()) {
-                dfs(neighbor, visited);
-            }
+void renameVertex() {
+    mainLabel.setString("Вы выбрали переименование вершины, нажмите на вершину для переименования");
+    window.unbind(sf::Mouse::Left);
+    window.bind(sf::Mouse::Left, [](sf::Event event) {
+        if (event.type == sf::Event::MouseButtonPressed) {
+            float x = event.mouseButton.x;
+            float y = event.mouseButton.y;
+            // TODO: Implement vertex renaming
         }
-    }
+    });
+}
 
-    // Остальные операции над графом
-
-    // Экспорт графа во внутренний формат программы
-    void exportToInternalFormat(const std::string& filename) {
-        std::ofstream outfile(filename);
-        if (outfile.is_open()) {
-            outfile << name << std::endl;
-            outfile << nodes.size() << std::endl;
-            for (Node* node : nodes) {
-                outfile << node->name << " " << node->content << std::endl;
-            }
-            outfile << edges.size() << std::endl;
-            for (Edge* edge : edges) {
-                outfile << edge->source->name << " " << edge->target->name << " " << edge->directed << " " << edge->color << std::endl;
-            }
-            outfile.close();
-            std::cout << "Graph exported to internal format: " << filename << std::endl;
-        } else {
-            std::cerr << "Unable to export graph to internal format." << std::endl;
+void changeColor() {
+    mainLabel.setString("Вы выбрали изменение цвета вершины, нажмите на вершину для изменения её цвета");
+    window.unbind(sf::Mouse::Left);
+    window.bind(sf::Mouse::Left, [](sf::Event event) {
+        if (event.type == sf::Event::MouseButtonPressed) {
+            float x = event.mouseButton.x;
+            float y = event.mouseButton.y;
+            // TODO: Implement changing vertex color
         }
-    }
+    });
+}
 
-    // Импорт графа из внутреннего формата программы
-    static Graph importFromInternalFormat(const std::string& filename) {
-        std::ifstream infile(filename);
-        if (infile.is_open()) {
-            std::string name;
-            infile >> name;
-            Graph graph(name);
+void changeTextColor() {
+    mainLabel.setString("Вы выбрали изменение цвета текста, нажмите на вершину для изменения цвета её текста");
+    window.unbind(sf::Mouse::Left);
+    window.bind(sf::Mouse::Left, [](sf::Event event) {
+        if (event.type == sf::Event::MouseButtonPressed) {
+            float x = event.mouseButton.x;
+            float y = event.mouseButton.y;
+            // TODO: Implement changing text color
+        }
+    });
+}
 
-            int numNodes;
-            infile >> numNodes;
-            for (int i = 0; i < numNodes; ++i) {
-                std::string nodeName, nodeContent;
-                infile >> nodeName >> nodeContent;
-                graph.addNode(nodeName);
-                Node* node = graph.nodeLookup[nodeName];
-                node->content = nodeContent;
-            }
+void changeEdgeColor() {
+    mainLabel.setString("Вы выбрали изменение цвета ребер, нажмите на ребро для изменения его цвета");
+    window.unbind(sf::Mouse::Left);
+    window.bind(sf::Mouse::Left, [](sf::Event event) {
+        if (event.type == sf::Event::MouseButtonPressed) {
+            float x = event.mouseButton.x;
+            float y = event.mouseButton.y;
+            // TODO: Implement changing edge color
+        }
+    });
+}
 
-            int numEdges;
-            infile >> numEdges;
-            for (int i = 0; i < numEdges; ++i) {
-                std::string sourceName, targetName, color;
-                bool directed;
-                infile >> sourceName >> targetName >> directed >> color;
-                graph.addEdge(sourceName, targetName, directed);
-                Edge* edge = graph.edges.back();
-                edge->color = color;
-            }
-
-            infile.close();
-            std::cout << "Graph imported from internal format: " << filename << std::endl;
-
-            return graph;
-        } else {
-            std::cerr << "Unable to import graph from internal format." << std::endl;
-            return Graph("");
+void deleteCanvas(sf::Event event) {
+    for (auto it = ovals.begin(); it != ovals.end(); ++it) {
+        sf::CircleShape& oval = *it;
+        if (oval.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
+            // TODO: Implement vertex deletion
+            ovals.erase(it);
+            break;
         }
     }
-
-    // Экспорт графа в текстовый формат
-    void exportToTextFormat(const std::string& filename) {
-        std::ofstream outfile(filename);
-        if (outfile.is_open()) {
-            outfile << "Graph: " << name << std::endl;
-            outfile << "Nodes:" << std::endl;
-            for (Node* node : nodes) {
-                outfile << "- Name: " << node->name << ", Content: " << node->content << std::endl;
-            }
-            outfile << "Edges:" << std::endl;
-            for (Edge* edge : edges) {
-                outfile << "- Source: " << edge->source->name << ", Target: " << edge->target->name << ", Directed: " << (edge->directed ? "true" : "false") << ", Color: " << edge->color << std::endl;
-            }
-            outfile.close();
-            std::cout << "Graph exported to text format: " << filename << std::endl;
-        } else {
-            std::cerr << "Unable to export graph to text format." << std::endl;
-        }
-    }
-
-    // Импорт графа из текстового формата
-    static Graph importFromTextFormat(const std::string& filename) {
-        std::ifstream infile(filename);
-        if (infile.is_open()) {
-            std::string line;
-            std::getline(infile, line); // Пропускаем первую строку
-            std::getline(infile, line); // Считываем имя графа
-            std::string graphName = line.substr(line.find(":") + 2);
-            Graph graph(graphName);
-
-            std::getline(infile, line); // Пропускаем строку "Nodes:"
-            while (std::getline(infile, line) && line != "Edges:") {
-                if (line.empty()) continue;
-                std::istringstream iss(line);
-                std::string nodeName, nodeContent;
-                std::string temp;
-                iss >> temp >> nodeName >> temp >> nodeContent;
-                graph.addNode(nodeName);
-                Node* node = graph.nodeLookup[nodeName];
-                node->content = nodeContent;
-            }
-
-            while (std::getline(infile, line)) {
-                if (line.empty()) continue;
-                std::istringstream iss(line);
-                std::string sourceName, targetName, color;
-                bool directed;
-                std::string temp;
-                iss >> temp >> sourceName >> temp >> targetName >> temp >> directed >> temp >> color;
-                graph.addEdge(sourceName, targetName, directed);
-                Edge* edge = graph.edges.back();
-                edge->color = color;
-            }
-
-            infile.close();
-            std::cout << "Graph imported from text format: " << filename << std::endl;
-
-            return graph;
-        } else {
-            std::cerr << "Unable to import graph from text format." << std::endl;
-            return Graph("");
-        }
-    }
-
-    // Остальные операции над графом
-
-    void printGraphInfo() {
-        std::cout << "Graph: " << name << std::endl;
-        std::cout << "Number of Nodes: " << nodes.size() << std::endl;
-        std::cout << "Number of Edges: " << edges.size() << std::endl;
-
-        // Печать степеней для всех вершин
-        std::cout << "Degrees for all Nodes: " << std::endl;
-        for (Node* node : nodes) {
-            int degree = getDegree(node);
-            std::cout << "- Node " << node->name << ": " << degree << std::endl;
-        }
-
-        // Печать матрицы инцидентности
-        std::cout << "Incidence Matrix: " << std::endl;
-        std::vector<std::vector<int>> incidenceMatrix(nodes.size(), std::vector<int>(edges.size(), 0));
-        for (int i = 0; i < edges.size(); ++i) {
-            Edge* edge = edges[i];
-            incidenceMatrix[getIndex(edge->source)][i] = 1;
-            if (!edge->directed) {
-                incidenceMatrix[getIndex(edge->target)][i] = 1;
-            }
-        }
-        for (const std::vector<int>& row : incidenceMatrix) {
-            for (int val : row) {
-                std::cout << val << " ";
-            }
-            std::cout << std::endl;
-        }
-
-        // Печать матрицы смежности
-        std::cout << "Adjacency Matrix: " << std::endl;
-        std::vector<std::vector<int>> adjacencyMatrix(nodes.size(), std::vector<int>(nodes.size(), 0));
-        for (Edge* edge : edges) {
-            int sourceIndex = getIndex(edge->source);
-            int targetIndex = getIndex(edge->target);
-            adjacencyMatrix[sourceIndex][targetIndex] = 1;
-            if (!edge->directed) {
-                adjacencyMatrix[targetIndex][sourceIndex] = 1;
-            }
-        }
-        for (const std::vector<int>& row : adjacencyMatrix) {
-            for (int val : row) {
-                std::cout << val << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    int getDegree(Node* node) {
-        int degree = 0;
-        for (Edge* edge : edges) {
-            if (edge->source == node || (!edge->directed && edge->target == node)) {
-                degree++;
-            }
-        }
-        return degree;
-    }
-
-    int getIndex(Node* node) {
-        for (int i = 0; i < nodes.size(); ++i) {
-            if (nodes[i] == node) {
-                return i;
-            }
-        }
-        return -1; // Вершина не найдена
-    }
-
-    // Остальные операции над графом
-};
+    // TODO: Implement edge deletion
+}
 
 int main() {
-    Graph graph("MyGraph");
-    graph.addNode("A");
-    graph.addNode("B");
-    graph.addNode("C");
-    graph.addNode("D");
-    graph.addEdge("A", "B");
-    graph.addEdge("B", "C");
-    graph.addEdge("C", "D");
-    graph.addEdge("D", "A", true);
+    window.create(sf::VideoMode(800, 600), "Graph Editor");
+    font.loadFromFile("arial.ttf");
 
-    graph.printGraphInfo();
+    mainLabel.setFont(font);
+    mainLabel.setCharacterSize(20);
+    mainLabel.setPosition(10, 10);
 
-    graph.exportToInternalFormat("graph.txt");
-    Graph importedGraph = Graph::importFromInternalFormat("graph.txt");
-    importedGraph.printGraphInfo();
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
 
-    graph.exportToTextFormat("graph.txt");
-    Graph importedTextGraph = Graph::importFromTextFormat("graph.txt");
-    importedTextGraph.printGraphInfo();
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Num1) {
+                    drawVertex();
+                } else if (event.key.code == sf::Keyboard::Num2) {
+                    drawEdge();
+                } else if (event.key.code == sf::Keyboard::Num3) {
+                    deleteVertex();
+                } else if (event.key.code == sf::Keyboard::Num4) {
+                    renameVertex();
+                } else if (event.key.code == sf::Keyboard::Num5) {
+                    changeColor();
+                } else if (event.key.code == sf::Keyboard::Num6) {
+                    changeTextColor();
+                } else if (event.key.code == sf::Keyboard::Num7) {
+                    changeEdgeColor();
+                }
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed) {
+                deleteCanvas(event);
+            }
+        }
+
+        window.clear();
+        window.draw(mainLabel);
+        window.draw(vertices);
+        for (const auto& oval : ovals) {
+            window.draw(oval);
+        }
+        window.display();
+    }
 
     return 0;
 }
