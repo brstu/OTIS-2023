1b544501f10d95127256b1e531b98b1117fdda22
Rudzetski Yauheni <ii002416@g.bstu.by>
2023-11-02 22:50:13 +0300

    FIXXXXXX

--- trunk/ii02416/task_02/src/lab2.cpp
+++ trunk/ii02416/task_02/src/lab2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> // Include the <algorithm> header for std::ranges::for_each
+#include <algorithm> 
 
 class pid
 {
@@ -21,7 +21,7 @@ public:
       return c_sig;
    }
 
-   void lin(double y_c, double u, double t, double dt, double setting)
+   void lin(double y_c, double u, double t, double dt, size_t setting) // Change the type of 'setting' to 'size_t'
    {
       double a = 0.925;
       double b = 0.75;
@@ -30,13 +30,13 @@ public:
       double k_d = 0.0006;
       double y_next = 0;
       std::vector<double> a1;
-      std::vector<double> a3(static_cast<size_t>(t), setting);
+      std::vector<double> a3(setting, static_cast<double>(t)); // Convert 't' to 'double'
       a1.push_back(y_c);
       for (int i = 1; i <= static_cast<int>(t); i++)
       {
          if (i % static_cast<int>(dt) == 0)
          {
-            double y = pid_c(k_p, k_i, k_d, setting, y_c, dt);
+            double y = pid_c(k_p, k_i, k_d, static_cast<double>(setting), y_c, dt); 
             y_next = a * y + b * u;
             a1.push_back(y_next);
             y_c = y_next;
@@ -49,24 +49,21 @@ public:
          }
       }
       std::cout << "value:\n";
-      for (double val : a1) 
+      for (double val : a1) // Range for-loop
       {
          std::cout << val << ",";
       }
       std::cout << "\n\nsignal:\n";
-      for (int i = 0; i < a1.size() - a2.size(); i++)
+      for (int ii = 0; ii < a1.size() - a2.size(); ii++)
       {
          a2.push_back(a2.back());
       }
-      for (double val : a2) 
+      for (double val : a2) // Range for-loop
       {
          std::cout << val << ",";
       }
       std::cout << "\n\nsetting:\n";
-      for (double val : a3) 
-      { 
-	std::cout << val << ","; 
-      } 
+      std::ranges::for_each(a3, [](double val) { std::cout << val << ","; }); 
    }
 };
 
@@ -74,7 +71,7 @@ int main()
 {
    pid T;
    std::cout << "setting:\n";
-   double setting;
+   size_t setting; // Change the type of 'setting' to 'size_t'
    std::cin >> setting;
    std::cout << "duration:\n";
    double duration;
@@ -82,6 +79,6 @@ int main()
    std::cout << "discretization value:\n";
    double dis_time;
    std::cin >> dis_time;
-   T.lin(0, 0, duration, dis_time, static_cast<size_t>(setting));
+   T.lin(0, 0, duration, dis_time, setting); // Pass 'setting' as a 'size_t' value
    return 0;
 }
