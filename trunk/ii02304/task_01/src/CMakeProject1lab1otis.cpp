e3537691237cd0232791758f526a6a6068e3d5aa
Gavrishuk Vladislav Romanovich <vgavrishuk43@gmail.com>
2023-11-02 21:57:39 +0300

    Update CMakeProject1lab1otis.cpp

--- trunk/ii02304/task_01/src/CMakeProject1lab1otis.cpp
+++ trunk/ii02304/task_01/src/CMakeProject1lab1otis.cpp
@@ -1,20 +1,21 @@
 ﻿#include <iostream>
 #include <cmath>
+#include <vector>
 #include <fstream>
 
 using namespace std;
 
-float a = 1.0;
-float b = 0.5;
-float c = 0.001;
-float d = 0.999;
-float ylin = 0;
+float const a = 1.0;
+float const b = 0.5;
+double const c = 0.001;
+double const d = 0.999;
+float const ylin = 0;
 float ylin1;
-float ulin = 1.0;
-float n = 0;
+float const ulin = 1.0;
+float const n = 0;
 float y;
 float y_prev;
-int t = 10;
+int const t = 10;
 
 void linmodel(float a, float b, float c, float d, float ylin, float ylin1, float ulin) {
 	float ylinmodel[1000];
@@ -28,7 +29,7 @@ void linmodel(float a, float b, float c, float d, float ylin, float ylin1, float
 	}
 };
 
-void nonlinear(float a, float b, float c, float d, float ulin, float y, float y_prev) {
+void nonlinear(float a, float b, double c, double d, float ulin, float y, float y_prev) {
 	float ynonlinmodel[1000];
 	cout << endl << "Nonlinear model:" << endl;
 	ynonlinmodel[0] = 0;
@@ -51,7 +52,8 @@ void nonlinear(float a, float b, float c, float d, float ulin, float y, float y_
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	float ylinmodel[1000], ynonlinmodel[1000];
+	vector <float> ylinmodel[1000];
+	vector <float> ynonlinmodel[1000];
 	linmodel(a, b, c, d, ylin, ylin1, ulin);
 	nonlinear(a, b, c, d, ulin, y, y_prev);
 	return 0;
