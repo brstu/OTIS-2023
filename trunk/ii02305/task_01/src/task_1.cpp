55d14045b5634ee22f23e06f109cd6faef8e5ea8
Gluharev Daniil Egorovich <egglukharev@gmail.com>
2023-11-02 11:00:28 +0300

    Add task_01

---
+++ trunk/ii02305/task_01/src/task_1.cpp
@@ -0,0 +1,29 @@
+﻿#include "task_1.h"
+#include <cmath>
+#include <iostream>
+
+double a = 0.5, b = 0.5, c = 0.66, d = 0.8, u = 4.0;
+
+double func2(double prev, double a, double b, double u) {
+	double next = a * prev + b * u;
+	return next;
+}
+
+double func3(double prev, double a, double b, double c, double d, double u) {
+	double next = a * prev - prev * prev * b + u * c + d * sin(u);
+	return next;
+}
+
+int main()
+{
+	double y1 = 0.6, prev1 = y1, prev2 = y1;
+	int t;
+	std::cout << "Enter t: ";
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cout << i << "; " << func2(prev1, a, b, u) << "; " << func3(prev2, a, b, c, d, u) << std::endl;
+		prev1 = func2(prev1, a, b, u);
+		prev2 = func3(prev2, a, b, c, d, u);
+	}
+	return 0;
+}
