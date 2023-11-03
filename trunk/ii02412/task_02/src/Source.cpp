8e027fa1f16537e9fa4a4e264cef0f50b9d554f2
ClideIaM <147812026+MsharVladislav@users.noreply.github.com>
2023-11-03 23:27:41 +0300

    Update Source.cpp
    Fix code

--- trunk/ii02412/task_02/src/Source.cpp
+++ trunk/ii02412/task_02/src/Source.cpp
@@ -4,65 +4,73 @@
 
 using namespace std;
 
-class Func {
-private:
-    double K;
-    double T;
-    double T_D;
-    double T_0;
+class Model
+{
+  private:
+	double A;
+	double B;
+	double C;
+	double D;
 
-    double q_0;
-    double q_1;
-    double q_2;
+	double p_0;
+	double p_1;
+	double p_2;
 
-    vector<double> q;
-    vector<double> e;
-    vector<double> y;
-    vector<double> u;
+	vector<double> p;
+	vector<double> r;
+	vector<double> z;
+	vector<double> v;
 
-public:
-    Func(double k, double t, double tD, double t0)
-        : K(k), T(t), T_D(tD), T_0(t0), q_0(K* (1 + T_D / T_0)),
-        q_1(-K * (1 + 2 * T_D / T_0 - T_0 / T)), q_2(K* T_D / T_0),
-        q({ q_0, q_1, q_2 }), e({ 0, 0, 0 }), y({ 0, 0, 0 }), u({ 1, 1 }) {}
+  public:
+	Model(double a, double b, double c, double d)
+		: A(a), B(b), C(c), D(d), p_0(A * (1 + C / D)),
+		  p_1(-A * (1 + 2 * C / D - D / B)), p_2(A * C / D),
+		  p({p_0, p_1, p_2}), r({0, 0, 0}), z({0, 0, 0}), v({1, 1}) {}
 
-    double sum() {
-        double sum = 0;
-        for (int i = 0; i < 3; i++) {
-            sum += q[i] * e[i];
-        }
-        return sum;
-    }
+	double total()
+	{
+		double total = 0;
+		for (int i = 0; i < 3; i++)
+		{
+			total += p[i] * r[i];
+		}
+		return total;
+	}
 
-    void nonlin(int time, double setpoint, double a = 0.5, double b = 0.3, double c = 0.9, double d = 0.7) {
-        for (int i = 0; i < time; i++) {
-            e[0] = setpoint - y[y.size() - 1];
-            e[1] = setpoint - y[y.size() - 2];
-            e[2] = setpoint - y[y.size() - 3];
-            u[0] = u[1] + sum();
-            y.push_back(a * y[y.size() - 1] - b * y[y.size() - 2] * y[y.size() - 2] + c * u[0] + d * sin(u[1]));
-            u[1] = u[0];
-        }
-    }
+	void nonlinear(int steps, double target, double x = 0.5, double y = 0.3, double w = 0.9, double s = 0.7)
+	{
+		for (int i = 0; i < steps; i++)
+		{
+			r[0] = target - z[z.size() - 1];
+			r[1] = target - z[z.size() - 2];
+			r[2] = target - z[z.size() - 3];
+			v[0] = v[1] + total();
+			z.push_back(x * z[z.size() - 1] - y * z[z.size() - 2] * z[z.size() - 2] + w * v[0] + s * sin(v[1]));
+			v[1] = v[0];
+		}
+	}
 
-    vector<double> getY() const {
-        return y;
-    }
+	vector<double> getZ() const
+	{
+		return z;
+	}
 };
 
-int main() {
-    double setpoint;
-    Func f(0.0001, 100, 100, 1);
+int main()
+{
+	double target;
+	Model m(0.0001, 100, 100, 1);
 
-    cout << "Enter the setpoint: ";
-    cin >> setpoint;
-    f.nonlin(100, setpoint);
-    vector<double> y_values = f.getY();
+	cout << "Введите целевое значение: ";
+	cin >> target;
+	m.nonlinear(100, target);
+	vector<double> z_values = m.getZ();
 
-    for (int i = 0; i < y_values.size(); i++) {
-        double scaledValue = y_values[i] * setpoint / y_values[y_values.size() - 1];
-        cout << i << " " << scaledValue << endl;
-    }
+	for (int i = 0; i < z_values.size(); i++)
+	{
+		double scaledValue = z_values[i] * target / z_values[z_values.size() - 1];
+		cout << i << " " << scaledValue << endl;
+	}
 
-    return 0;
+	return 0;
 }
