3fd440e492ef270416137e3ed96bbcd0de60ffc8
Gluharev Daniil Egorovich <egglukharev@gmail.com>
2023-11-05 11:49:41 +0300

    Update task_1.cpp

--- trunk/ii02305/task_01/src/task_1.cpp
+++ trunk/ii02305/task_01/src/task_1.cpp
@@ -7,12 +7,12 @@ const double c = 0.66;
 const double d = 0.8;
 const double u = 4.0;
 
-double func2(double prev, a, b, u) {
+double func2(double prev) {
 	double next = a * prev + b * u;
 	return next;
 }
 
-double func3(double prev, a, b, c, d, u) {
+double func3(double prev) {
 	double next = a * prev - prev * prev * b + u * c + d * sin(u);
 	return next;
 }
@@ -24,9 +24,9 @@ int main()
 	std::cout << "Enter t: ";
 	std::cin >> t;
 	for (int i = 0; i < t; i++) {
-		std::cout << i << "; " << func2(prev1, a, b, u) << "; " << func3(prev2, a, b, c, d, u) << std::endl;
-		prev1 = func2(prev1, a, b, u);
-		prev2 = func3(prev2, a, b, c, d, u);
+		std::cout << i << "; " << func2(prev1) << "; " << func3(prev2) << std::endl;
+		prev1 = func2(prev1);
+		prev2 = func3(prev2);
 	}
 	return 0;
 }
