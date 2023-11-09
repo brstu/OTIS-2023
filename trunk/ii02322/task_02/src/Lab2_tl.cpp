abb8b98827ec722f9b0b9048616be2608cd89f21
Tutina Elizaveta Dmitrievna <145442986+Eliza0756@users.noreply.github.com>
2023-11-09 13:20:08 +0300

    Update Lab2_tl.cpp

--- trunk/ii02322/task_02/src/Lab2_tl.cpp
+++ trunk/ii02322/task_02/src/Lab2_tl.cpp
@@ -17,13 +17,13 @@ struct PID {
 	double u0;
 	double u1;
 
-	void const next_val(double& x0, double& x1, double next) 
+	void next_val(double& x0, double& x1, double next) const
 	{
 		x0 = x1;
 		x1 = next;
 	}
 
-	void const next_val1(double& x0, double& x1, double& x2, double next) 
+	void next_val1(double& x0, double& x1, double& x2, double next) const
 	{
 		x0 = x1;
 		x1 = x2;
