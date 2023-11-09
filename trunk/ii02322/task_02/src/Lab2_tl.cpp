e481f78c4ab9eeeb9266b76baa13af6d1e4a3612
Tutina Elizaveta Dmitrievna <145442986+Eliza0756@users.noreply.github.com>
2023-11-09 13:36:03 +0300

    Update Lab2_tl.cpp

--- trunk/ii02322/task_02/src/Lab2_tl.cpp
+++ trunk/ii02322/task_02/src/Lab2_tl.cpp
@@ -23,42 +23,61 @@ struct PID {
 		x1 = next;
 	}
 
-	void next_val1(double& x0, double& x1, double& x2, double next) const
+	void next_val1(double& x0, double& x1, double& x2, double next) 
 	{
 		x0 = x1;
 		x1 = x2;
 		x2 = next;
 	}
 
-	double K = 0.9;
-	double T = 1.1;
-	double TD = 1.01;
-	double T0 = 1.34;
-
-	double funk0(double K, double TD, double T0) 
+	double K ;
+	double T ;
+	double TD;
+	double T0;
+	double funk0(double K, double TD, double T0) const 
 	{
+		 K = 0.9;
+		 TD = 1.01;
+		 T0 = 1.34;
 		double q0 = K * (1 + TD / T0);
 		return q0;
 
 	}
 
-	double funk1(double K, double TD, double T0,double T)
+	double funk1(double K, double TD, double T0,double T) const
 	{
-		double q1 = -K * (1 + 2 * TD / T0 - T0 / T);		
+		 K = 0.9;
+		 T = 1.1;
+		 TD = 1.01;
+		 T0 = 1.34;
+		 double q1 = -K * (1 + 2 * TD / T0 - T0 / T);		
 		return q1;
 
 	}
 
-	double funk2(double K, double TD, double T0)
+	double funk2(double K, double TD, double T0) const
 	{
 
+		K = 0.9;
+		TD = 1.01;
+		T0 = 1.34;
 		double q2 = K * TD / T0;
 		return q2;
 	}	
-		
+	double A;
+	double B;
+	double C;
+	double D;
+	double U;
+	double W;
 	void function_cout(double val_y1, double val_y0)
 	{
-		double A = 0.5, B = 0.5, C = 0.35, D = 1.1, U = 4.1, W = 30;;
+		A = 0.5;
+		B = 0.5;
+		C = 0.35;
+		D = 1.1;
+		U = 4.1;
+		W = 30;
 		int i = 0;
 		y0 = val_y0;
 		y1 = val_y1;
