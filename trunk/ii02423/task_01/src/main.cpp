fc55539edfbdd4721e63beb234283f945b3bf6b7
Якимовец Евгений Геннадьевич <eugeneyakimovets@gmail.com>
2023-11-10 14:07:31 +0300

    Rererecomment
    nothing N4

--- trunk/ii02423/task_01/src/main.cpp
+++ trunk/ii02423/task_01/src/main.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
 class ModelParameters
 {
-    double a = 0.3;
-    double b = 0.47;
-    double c = 0.13;
-    double d = 0.47;
-    double u = 1.1;
+public:
+    const double a = 0.3;
+    const double b = 0.47;
+    const double c = 0.13;
+    const double d = 0.47;
+    const double u = 1.1;
     double y = 0.1;
-
     friend double linear_model(int t, int n);
     friend double unlinear_model(int t);
-
-public:
-    ModelParameters() : y(0.1) {}
 };
 
 ModelParameters params;
@@ -35,27 +31,26 @@ double linear_model(int t, int n)
 
 double unlinear_model(int t)
 {
-    for (int i = 1; i <= t; i++)
+    int i = 1;
+    while (i <= t)
     {
         double y_prev = params.y;
         double u_prev = params.u;
         double result = params.a * params.y - params.b * pow(y_prev, 2) + params.c * params.u + params.d * sin(u_prev);
         params.y = result;
         cout << params.y << endl;
+        i++;
     }
     return params.y;
 }
 
 int main()
 {
-    cout << "Linel model" << endl;
-    cout << linear_model(10, 0) << endl;
-
-    cout << "\n---------------\n"
+    cout << "Linear model" << endl;
+    linear_model(10, 0);
+    cout << "\n----------------\n"
          << endl;
-
-    cout << "Unlinel model" << endl;
-    cout << unlinear_model(10) << endl;
-
+    cout << "Nonlinear model" << endl;
+    unlinear_model(10);
     return 0;
 }
