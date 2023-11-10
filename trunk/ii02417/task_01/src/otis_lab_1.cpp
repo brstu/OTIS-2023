24443f5520de856e2183446ac0ed7a57e88b669c
Ilya Suprunovich <suprunovichilja@gmail.com>
2023-11-10 13:59:46 +0300

    update

--- trunk/ii02417/task_01/src/otis_lab_1.cpp
+++ trunk/ii02417/task_01/src/otis_lab_1.cpp
@@ -1,46 +1,46 @@
 ﻿#include <iostream>
-#include<cmath>
+#include <cmath>
+#include <array>
+#include <vector>
 
-
-
- void LinearModel() {
+void LinearModel() {
     std::cout << "Linear Model" << std::endl;
     double yT = 1;
     double uT = 0.5;
     int time = 15;
     double a = 1.2;
     double b = 0.7;
-    double arrayLinearY[15]{};
+    std::vector<double> arrayLinearY(time);
 
     for (int i = 0; i < time; i++) {
         yT = a * yT + b * uT;
         arrayLinearY[i] = yT;
         std::cout << yT << std::endl;
     }
- }
+}
 
- void NonLinearModel() {
-     std::cout << "Non-Linear Model" << std::endl;
-     double yT = 1.5;
-     double uT = 0.5;
-     double prevYT = 0;
-     int time = 15;
-     double a = 1.2;
-     double b = 0.7;
-     double c = -0.32;
-     double d = 0.53;
-     double arrayNonLinearY[15]{};
+void NonLinearModel() {
+    std::cout << "Non-Linear Model" << std::endl;
+    double yT = 1.5;
+    double uT = 0.5;
+    double prevYT = 0;
+    int time = 15;
+    double a = 1.2;
+    double b = 0.7;
+    double c = -0.32;
+    double d = 0.53;
+    std::vector<double> arrayNonLinearY(time);
 
-     for (int i = 0; i < time; i++) {
-         yT = a * yT - b * pow(prevYT, 2) + c * uT + d * sin(uT);
-         arrayNonLinearY[i] = yT;
-         std::cout << yT << std::endl;
-     }
- }
+    for (int i = 0; i < time; i++) {
+        yT = a * yT - b * pow(prevYT, 2) + c * uT + d * sin(uT);
+        arrayNonLinearY[i] = yT;
+        std::cout << yT << std::endl;
+        prevYT = yT;
+    }
+}
 
- int main()
- {
-     LinearModel();
-     NonLinearModel();
- }
-
+int main() {
+    LinearModel();
+    NonLinearModel();
+    return 0;
+}
