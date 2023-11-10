ed89171521410ee902b5c8634491738aa5d29697
Dmitry Rekun <ii002415@g.bstu.by>
2023-11-10 13:34:16 +0300

    fix

--- trunk/ii02415/task_01/src/otis_lab1.cpp
+++ trunk/ii02415/task_01/src/otis_lab1.cpp
@@ -1,42 +1,40 @@
 ﻿#include <iostream>
-#include <math.h>
-using namespace std;
+#include <cmath>
 
 class Perem {
 public:
     void arry1() {
-        cout << "LineModel" << endl;
+        std::cout << "LineModel" << std::endl;
         float Yt = 3.0;
         float Ut = 0.85;
-        int time = 10;
-        float arrLine[10];
+        const int time = 10;
+        float arrLine[time];
         for (int i = 0; i < time; i++) {
-
             Yt = a * Yt + b * Ut;
             arrLine[i] = Yt;
-            cout << Yt << endl;
+            std::cout << Yt << std::endl;
         }
     }
 
     void arry2() {
-        cout << "NoLineModel" << endl;
+        std::cout << "NoLineModel" << std::endl;
         float Yt = 2.0;
         float Ut = 0.45;
         float Prev_Yt = 1;
-        int time = 10;
-        float arrNoLine[10];
+        const int time = 10;
+        float arrNoLine[time];
         for (int i = 0; i < time; i++) {
-            Yt = a * Yt - b * pow(Prev_Yt, 2) + c * Ut + d * sin(Ut);
+            Yt = a * Yt - b * std::pow(Prev_Yt, 2) + c * Ut + d * std::sin(Ut);
             arrNoLine[i] = Yt;
-            cout << Yt << endl;
+            std::cout << Yt << std::endl;
         }
     }
 
 private:
-    const int a = 4;
-    const int b = 2;
-    const int c = 3;
-    const int d = 6;
+    const float a = 4.0;
+    const float b = 2.0;
+    const float c = 3.0;
+    const float d = 6.0;
 };
 
 int main() {
