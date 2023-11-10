211ff64b794775f2f372bc00fc4b0c6a7c03eba0
Ilya Suprunovich <suprunovichilja@gmail.com>
2023-11-10 13:47:45 +0300

    update

--- trunk/ii02417/task_01/src/otis_lab_1.cpp
+++ trunk/ii02417/task_01/src/otis_lab_1.cpp
@@ -5,12 +5,12 @@
 
  void LinearModel() {
     std::cout << "Linear Model" << std::endl;
-    float yT = 1;
-    float uT = 0.5;
+    double yT = 1;
+    double uT = 0.5;
     int time = 15;
-    float a = 1.2;
-    float b = 0.7;
-    float arrayLinearY[15];
+    double a = 1.2;
+    double b = 0.7;
+    double arrayLinearY[15];
 
     for (int i = 0; i < time; i++) {
         yT = a * yT + b * uT;
@@ -21,15 +21,15 @@
 
  void NonLinearModel() {
      std::cout << "Non-Linear Model" << std::endl;
-     float yT = 1.5;
-     float uT = 0.5;
-     float prevYT = 0;
+     double yT = 1.5;
+     double uT = 0.5;
+     double prevYT = 0;
      int time = 15;
-     float a = 1.2;
-     float b = 0.7;
-     float c = -0.32;
-     float d = 0.53;
-     float arrayNonLinearY[15];
+     double a = 1.2;
+     double b = 0.7;
+     double c = -0.32;
+     double d = 0.53;
+     double arrayNonLinearY[15];
 
      for (int i = 0; i < time; i++) {
          yT = a * yT - b * pow(prevYT, 2) + c * uT + d * sin(uT);
