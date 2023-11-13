a49efd218f4c028056e0a53d56099461d048902f
Krupich_Daniil <144598554+Duferig@users.noreply.github.com>
2023-11-13 14:29:04 +0300

    Update otis2.cpp

--- trunk/ii02407/task_02/src/otis2.cpp
+++ trunk/ii02407/task_02/src/otis2.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-
 int main() {
     setlocale(LC_ALL, "ru");
     int K0 = 1;
@@ -17,35 +16,24 @@ int main() {
     double P = 0.01;
     double TD = 1.7;
     double j = 1.5;
-
     double l1 = 0;
     double l2 = 0;
     double en = 0;
-
     std::cout << "Íåëèíåéíàÿ ìîäåëü: \n" << std::endl;
-
     for (int k = K0; k <= time; k++) {
         double q0 = Q * (1 + TD / k);
         double q1 = -Q * (1 + 2 * TD / k - k / P);
         double q2 = Q * TD / k;
-
         l1 = j - y_nonLin1;
         l2 = j - y_nonLin2;
-
         double y_nonlin3 = alpha * y_nonLin2 - beta * pow(std::abs(y_nonLin1), 2) + gamma * t_nonLin1 + delta * sin(t_nonLin0);
-
         y_nonLin1 = y_nonLin2;
         y_nonLin2 = y_nonlin3;
-
         en = j - y_nonlin3;
-
         double t_nonlin_n = t_nonLin1 + q0 * en + q1 * l2 + q2 * l1;
-
         t_nonLin0 = t_nonLin1;
         t_nonLin1 = t_nonlin_n;
-
         std::cout << k << "\t" << y_nonlin3 << "\t:\t" << en << std::endl;
     }
-
     return 0;
 }
