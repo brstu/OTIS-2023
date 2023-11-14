2be00d55691b8a3cbb88a9eb6a286cb24a6b4236
AlenaSokol <alena_sokolovskau@mail.ru>
2023-11-14 15:02:56 +0300

    add task_1

--- trunk/II02319/task_01/src/main.cpp
+++ trunk/II02319/task_01/src/main.cpp
@@ -1,59 +1,76 @@
+
+
+
 #include <iostream>
 #include <fstream>
 #include <cmath>
-
 using namespace std;
 
-class Obscure {
+class LinearModel {
 private:
-    const double a = 0.5;
-    const double b = 0.5;
-    const double c = 0.5;
-    const double d = 0.5;
+    double a, b;
+    ofstream file;
 
-    int x = 1;
-    int y = 100;
-    int z = 1;
 public:
-    void obfuscateLinear(double var) {
-        ofstream dataFile("linear_data.txt", ios::out);
-        cout << "Executing obscured linear model:"<<endl;
-        for (int i = x; i <= y; i++) {
-            dataFile << i << " " << var << endl;
-            cout << var << endl;
-            var = a * var + b * z;
+    LinearModel(double a_val, double b_val) : a(a_val), b(b_val) {
+        file.open("linear_data.txt");
+        file.clear();
+    }
+
+    void simulate(double y, double u, double t, double i = 1) {
+        if (i != t) {
+            file << i << " " << y << endl;
+            cout << y << endl;
+            simulate(a * y + b * u, u, t, i + 1);
+        } else {
+            cout << "End of linear simulation\n";
         }
     }
 
-    void scrambleNonlinear(double var, double var2) {
-        ofstream dataFile2("nonlinear_data.txt", ios::out);
-        cout << "Executing obscured nonlinear model:"<<endl;
-        for (int i = x; i <= y; i++) {
-            dataFile2 << i << " " << var << endl;
-            cout << var << endl;
-            if (i == 1) {
-                var = a * var - b * var2 * var2 + c * 0 + d * sin(0);
-            } else {
-                var = a * var - b * var2 * var2 + c * z + d * sin(z);
-            }
-            var2 = var;
+    ~LinearModel() {
+        file.close();
+    }
+};
+
+class NonLinearModel {
+private:
+    double a, b, c, d;
+    ofstream file;
+
+public:
+    NonLinearModel(double a_val, double b_val, double c_val, double d_val)
+            : a(a_val), b(b_val), c(c_val), d(d_val) {
+        file.open("nonlinear_data.txt");
+        file.clear();
+    }
+
+    void simulate(double y, double y1, double u, double t, double i = 1) {
+        if (i != t) {
+            file << i << " " << y << endl;
+            cout << y << endl;
+            double nextY = a * y - b * y1 * y1 + c * u + d * sin(u);
+            simulate(nextY, y, u, t, i + 1);
+        } else {
+            cout << "End of nonlinear simulation\n";
         }
+    }
+
+    ~NonLinearModel() {
+        file.close();
     }
 };
 
 int main() {
-    double var = 0;
-    ofstream linearFile("linear_data.txt");
-    Obscure linearFunctions;
-    linearFunctions.obfuscateLinear(var);
-    linearFile.close();
+    const double a = 0.5, b = 0.5, c = 0.5, d = 0.5;
+    double y = 0, u = 1, t = 100;
 
-    var = 0.05;
-    double var2 = 0.2;
-    ofstream nonlinearFile("nonlinear_data.txt");
-    Obscure nonlinearFunctions;
-    nonlinearFunctions.scrambleNonlinear(var, var2);
-    nonlinearFile.close();
+    cout << "Linear model simulation:\n";
+    LinearModel linear(a, b);
+    linear.simulate(y, u, t);
+
+    cout << "\nNonlinear model simulation:\n";
+    NonLinearModel nonlinear(a, b, c, d);
+    nonlinear.simulate(y, y, u, t);
 
     return 0;
 }
