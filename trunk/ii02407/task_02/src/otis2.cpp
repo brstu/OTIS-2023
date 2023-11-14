daae67dbbbfd660c92247d4489a3509925ef4862
Krupich_Daniil <144598554+Duferig@users.noreply.github.com>
2023-11-14 09:07:37 +0300

    Update otis2.cpp

--- trunk/ii02407/task_02/src/otis2.cpp
+++ trunk/ii02407/task_02/src/otis2.cpp
@@ -1,39 +1,65 @@
+
 #include <iostream>
 #include <cmath>
+
+struct NonLinearModelParams {
+    double K0;
+    double time;
+    double alpha;
+    double beta;
+    double gamma;
+    double delta;
+    double y_nonLin1;
+    double y_nonLin2;
+    double t_nonLin1;
+    double t_nonLin0;
+    double Q;
+    double P;
+    double TD;
+    double j;
+};
+
+void calculateNonLinearModelParams(double k, const NonLinearModelParams& params) {
+    double q0 = params.Q * (1 + params.TD / k);
+    double q1 = -params.Q * (1 + 2 * params.TD / k - k / params.P);
+    double q2 = params.Q * params.TD / k;
+    double l1 = params.j - params.y_nonLin1;
+    double l2 = params.j - params.y_nonLin2;
+    double y_nonlin3 = params.alpha * params.y_nonLin2 - params.beta * std::pow(std::abs(params.y_nonLin1), 2) + params.gamma * params.t_nonLin1 + params.delta * std::sin(params.t_nonLin0);
+    double en = params.j - y_nonlin3;
+    double t_nonlin_n = params.t_nonLin1 + q0 * en + q1 * l2 + q2 * l1;
+    l1 = l2;
+    l2 = en;
+    params.t_nonLin0 = params.t_nonLin1;
+    params.t_nonLin1 = t_nonlin_n;
+    std::cout << k << "\t" << y_nonlin3 << "\t:\t" << en << std::endl;
+}
+
+void calculateNonLinearModel(const NonLinearModelParams& params) {
+    std::cout << "Модель нелинейной системы: \n" << std::endl;
+    for (double k = params.K0; k <= params.time; k++) {
+        calculateNonLinearModelParams(k, params);
+    }
+}
+
 int main() {
     setlocale(LC_ALL, "ru");
-    int K0 = 1;
-    double time = 10;
-    double alpha = 0.002;
-    double beta = 0.005;
-    double gamma = 0.45;
-    double delta = 0.007;
-    double y_nonLin1 = 0.2;
-    double y_nonLin2 = 0.04;
-    double t_nonLin1 = 0.1;
-    double t_nonLin0 = 0.0025;
-    double Q = 0.04;
-    double P = 0.01;
-    double TD = 1.7;
-    double j = 1.5;
-    double l1 = 0;
-    double l2 = 0;
-    double en = 0;
-    std::cout << "Нелинейная модель: \n" << std::endl;
-    for (int k = K0; k <= time; k++) {
-        double q0 = Q * (1 + TD / k);
-        double q1 = -Q * (1 + 2 * TD / k - k / P);
-        double q2 = Q * TD / k;
-        l1 = j - y_nonLin1;
-        l2 = j - y_nonLin2;
-        double y_nonlin3 = alpha * y_nonLin2 - beta * pow(std::abs(y_nonLin1), 2) + gamma * t_nonLin1 + delta * sin(t_nonLin0);
-        y_nonLin1 = y_nonLin2;
-        y_nonLin2 = y_nonlin3;
-        en = j - y_nonlin3;
-        double t_nonlin_n = t_nonLin1 + q0 * en + q1 * l2 + q2 * l1;
-        t_nonLin0 = t_nonLin1;
-        t_nonLin1 = t_nonlin_n;
-        std::cout << k << "\t" << y_nonlin3 << "\t:\t" << en << std::endl;
-    }
+    std::cout << std::endl;
+    NonLinearModelParams params;
+    params.K0 = 1;
+    params.time = 10;
+    params.alpha = 0.002;
+    params.beta = 0.005;
+    params.gamma = 0.45;
+    params.delta = 0.007;
+    params.y_nonLin1 = 0.2;
+    params.y_nonLin2 = 0.04;
+    params.t_nonLin1 = 0.1;
+    params.t_nonLin0 = 0.0025;
+    params.Q = 0.04;
+    params.P = 0.01;
+    params.TD = 1.7;
+    params.j = 1.5;
+    calculateNonLinearModel(params);
     return 0;
 }
