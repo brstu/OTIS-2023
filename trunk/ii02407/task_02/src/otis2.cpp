d8e88b8f6141741f361f54ddcd2ad2367a8a8173
FeRmEr214 <holdik6687@gmail.com>
2023-11-14 10:25:27 +0300

    Fix

--- trunk/ii02407/task_02/src/otis2.cpp
+++ trunk/ii02407/task_02/src/otis2.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 
 int main() {
-<<<<<<< HEAD
     // pid setups
     double kp = 10;
     double ki = 1;
@@ -15,8 +14,6 @@ int main() {
     double prevErr = 0;
 
     // models setups
-=======
->>>>>>> 8ae2172f2d44ec9be324000edd6318bb1a0a862e
     double a = 0.5;
     double b = 0.04;
 
@@ -43,26 +40,5 @@ int main() {
         std::cout << result << std::endl;
     }
 
-<<<<<<< HEAD
-=======
-    setlocale(LC_ALL, "ru");
-    std::cout << std::endl;
-    NonLinearModelParams params;
-    params.K0 = 1;
-    params.time = 10;
-    params.alpha = 0.002;
-    params.beta = 0.005;
-    params.gamma = 0.45;
-    params.delta = 0.007;
-    params.y_nonLin1 = 0.2;
-    params.y_nonLin2 = 0.04;
-    params.t_nonLin1 = 0.1;
-    params.t_nonLin0 = 0.0025;
-    params.Q = 0.04;
-    params.P = 0.01;
-    params.TD = 1.7;
-    params.j = 1.5;
-    calculateNonLinearModel(params);
->>>>>>> 8ae2172f2d44ec9be324000edd6318bb1a0a862e
     return 0;
 }
