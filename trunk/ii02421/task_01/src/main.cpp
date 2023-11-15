13adb36466f24eac9dd9ab9ab69005c9a5770f00
Черник Владислав <jeffrie.c1o@yandex.ru>
2023-11-15 22:37:38 +0300

    Update main.cpp

--- trunk/ii02421/task_01/src/main.cpp
+++ trunk/ii02421/task_01/src/main.cpp
@@ -7,44 +7,50 @@ const double u = 2.05;
 const int t = 50;
 
 void linearFunction(int t, double a, double b, double u){
-    double y[t];
+    double list[t];
 
     for(int i = 0; i < t; i++) {
         if (i <= 0) {
-            y[i] = 0;
+            list[i] = a * 0 + b * u;
         } else {
-            y[i] = a * y[i - 1] + b * u;
+            list[i] = a * list[i - 1] + b * u;
         }
     }
 
     std::cout << "Linear:\n" << std::endl;
 
     for(int i = 0; i < t; i++)
-        std::cout << y[i] << std::endl;
+        std::cout << list[i] << std::endl;
 }
 
 void nonLinearFunction(int t, double a, double b, double c, double d, double u){
-    double y[t];
+    double list[t];
 
     for(int i = 0; i<t; i++) {
         if (i > 0) {
-            y[i] = a * y[i - 1] - b * pow(y[i - 2], 2) + c * u + d * sin(u);
+            list[i] = a * list[i - 1] - b * pow(list[i - 2], 2) + c * u + d * sin(u);
         } else {
-            y[i] = a * 0 - b * pow(0, 2) + c * u + d * sin(u);
+            list[i] = a * 0 - b * pow(0, 2) + c * u + d * sin(u);
         }
     }
 
     std::cout << "\nNon Linear:\n" << std::endl;
 
     for(int i = 0; i < t; i++) {
-        std::cout << y[i] << std::endl;
+        std::cout << list[i] << std::endl;
     }
     std::cout << std::endl;
 }
 
 int main() {
-    linearFunction(t, lin[0], lin[1], u);
-    nonLinearFunction(t, nonLin[0], nonLin[1], nonLin[2], nonLin[3], u);
+    for(int i = 0; i < 2; i++){
+        if(i == 0){
+            linearFunction(t, lin[0], lin[1], u);
+        }
+        if(i == 1){
+            nonLinearFunction(t, nonLin[0], nonLin[1], nonLin[2], nonLin[3], u);
+        }
+    }
 
     return 0;
 }
