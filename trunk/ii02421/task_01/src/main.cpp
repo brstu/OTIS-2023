57765e4a830868590f752a79740ff2326301c125
Черник Владислав <jeffrie.c1o@yandex.ru>
2023-11-15 22:31:49 +0300

    Update main.cpp

--- trunk/ii02421/task_01/src/main.cpp
+++ trunk/ii02421/task_01/src/main.cpp
@@ -1,52 +1,50 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 
-using namespace std;
-
 const double lin[2] = {0.34, 2};
-const double nonLin[4] = {0.87, 0.45, 0.64, -0.5};
-const double u = 1.95;
+const double nonLin[4] = {0.56, 0.91, 0.3, -0.1};
+const double u = 2.05;
 const int t = 50;
 
 void linearFunction(int t, double a, double b, double u){
-   vector <double> y(t);
+    double y[t];
 
-   for(int i = 0; i < t; i++) {
-       if (i <= 0) {
-           y[i] = 0;
-       } else {
-           y[i] = a * y[i - 1] + b * u;
-       }
-   }
+    for(int i = 0; i < t; i++) {
+        if (i <= 0) {
+            y[i] = 0;
+        } else {
+            y[i] = a * y[i - 1] + b * u;
+        }
+    }
 
-   cout << "Linear:\n";
+    std::cout << "Linear:\n" << std::endl;
 
-   for(int i = 0; i < t; i++)
-      cout << y[i] << endl;
+    for(int i = 0; i < t; i++)
+        std::cout << y[i] << std::endl;
 }
 
 void nonLinearFunction(int t, double a, double b, double c, double d, double u){
-   vector <double> y(t);
+    double y[t];
 
-   for(int i = 0; i<t; i++) {
-       if (i <= 0) {
-           y[i] = a * 0 - b * pow(0, 2) + c * u + d * sin(u);
-       } else {
-           y[i] = a * y[i - 1] - b * pow(y[i - 2], 2) + c * u + d * sin(u);
-       }
-   }
+    for(int i = 0; i<t; i++) {
+        if (i > 0) {
+            y[i] = a * y[i - 1] - b * pow(y[i - 2], 2) + c * u + d * sin(u);
+        } else {
+            y[i] = a * 0 - b * pow(0, 2) + c * u + d * sin(u);
+        }
+    }
 
-   cout << "\nNon Linear:\n" << endl;
+    std::cout << "\nNon Linear:\n" << std::endl;
 
-   for(int i = 0; i < t; i++)
-      cout << y[i] << endl;
-   cout << endl;
+    for(int i = 0; i < t; i++) {
+        std::cout << y[i] << std::endl;
+    }
+    std::cout << std::endl;
 }
 
 int main() {
-   linearFunction(t, lin[0], lin[1], u);
-   nonLinearFunction(t, nonLin[0], nonLin[1], nonLin[2], nonLin[3], u);
+    linearFunction(t, lin[0], lin[1], u);
+    nonLinearFunction(t, nonLin[0], nonLin[1], nonLin[2], nonLin[3], u);
 
-   return 0;
+    return 0;
 }
