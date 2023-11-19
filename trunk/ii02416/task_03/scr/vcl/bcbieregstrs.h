c3f0d445382567bd1831922eb163da85261d108a
Rudzetski Yauheni <144435170+RuuuuuD3@users.noreply.github.com>
2023-11-19 15:43:58 +0300

    upd

--- trunk/ii02416/task_03/scr/vcl/bcbieregstrs.h
+++
@@ -1,2 +0,0 @@
-#define Page_Servers 101
-#define Page_Internet 102
--- trunk/ii02416/task_03/scr/vcl/vcl_condefs.h
+++
@@ -1,5 +0,0 @@
-#ifndef __VCL_CONDEFS_H__
-#define __VCL_CONDEFS_H__
-#pragma message "vcl_condefs.h has been deprecated. Please include <System.hpp> or <vcl.h>"
-#include <condefs.h>
-#endif /* __VCL_CONDEFS_H__ */
