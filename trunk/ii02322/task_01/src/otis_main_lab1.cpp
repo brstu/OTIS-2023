9590c72e2ae20ac07bf76bcb60f7a17cf7e05441
Швороб Владислав Андреевич <136325247+Ampersand04@users.noreply.github.com>
2023-11-20 22:17:47 +0300

    fix

--- trunk/ii02322/task_01/src/otis_main_lab1.cpp
+++ trunk/ii02322/task_01/src/otis_main_lab1.cpp
@@ -19,7 +19,7 @@ public:
     Solution() : a(0.6), b(0.5), c(0.62), d(1.1), i(0) {}
 
     double func1(double y, double u) const;
-    double func2(double y, double u, double y1);
+    double func2(double y, double u, double y1) const;
     void input();
     void output(ofstream& outFile) const;
 };
@@ -28,7 +28,7 @@ double Solution::func1(double y, double u) const {
     return a * y + b * u;
 }
 
-double Solution::func2(double y, double u, double y1) {
+double Solution::func2(double y, double u, double y1) const {
     return a * y - b * pow(y1, 2) + c * u + d * sin(u);
 }
 
@@ -39,13 +39,17 @@ void Solution::input() {
 void Solution::output(ofstream& outFile) const {
     outFile << i << " " << y1 << " " << y1 << endl;
     double tempY1 = func1(y1, u);
+
     y2 = func2(tempY1, u, y1);
+
     i++;
     outFile << i << " " << tempY1 << " " << y2 << endl;
 
     for (int j = 2; j < 100; j++) {
         tempY1 = func1(tempY1, u);
+
         y2 = func2(y2, u, tempY1);
+
         outFile << j << " " << tempY1 << " " << y2 << endl;
     }
 }
