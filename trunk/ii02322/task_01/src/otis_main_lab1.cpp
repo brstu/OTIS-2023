45d8f3bd3ddb60f8af8718a06d617703afff443a
Швороб Владислав Андреевич <136325247+Ampersand04@users.noreply.github.com>
2023-11-20 22:26:16 +0300

    Fix

--- trunk/ii02322/task_01/src/otis_main_lab1.cpp
+++ trunk/ii02322/task_01/src/otis_main_lab1.cpp
@@ -7,16 +7,18 @@ using namespace std;
 class Solution {
 private:
     double y1;
-    mutable double y2;  // Make y2 mutable
+    double y2;
     double u;
     double a;
     double b;
     double c;
     double d;
-    mutable int i;  // Make i mutable
+    int i;
 
 public:
-    Solution() : a(0.6), b(0.5), c(0.62), d(1.1), i(0) {}
+    // Constructor with initializer list
+    Solution()
+        : y1(0.0), y2(0.0), u(0.0), a(0.6), b(0.5), c(0.62), d(1.1), i(0) {}
 
     double func1(double y, double u) const;
     double func2(double y, double u, double y1) const;
@@ -40,20 +42,15 @@ void Solution::output(ofstream& outFile) const {
     outFile << i << " " << y1 << " " << y1 << endl;
     double tempY1 = func1(y1, u);
 
-    // Update y2 using const func2
     y2 = func2(tempY1, u, y1);
 
-    // Increment i
     i++;
 
     outFile << i << " " << tempY1 << " " << y2 << endl;
 
     for (int j = 2; j < 100; j++) {
         tempY1 = func1(tempY1, u);
-
-        // Update y2 using const func2
         y2 = func2(y2, u, tempY1);
-
         outFile << j << " " << tempY1 << " " << y2 << endl;
     }
 }
