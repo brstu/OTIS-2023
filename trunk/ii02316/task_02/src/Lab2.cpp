28fbf1cb4dfa653ca6ef7f08d42fb73dcec04d0d
Медведь Павел <118657761+Dizzers@users.noreply.github.com>
2023-11-21 19:09:09 +0300

    fix problems

--- trunk/ii02316/task_02/src/Lab2.cpp
+++ trunk/ii02316/task_02/src/Lab2.cpp
@@ -12,13 +12,20 @@ private:
     double T_D = 100;
     double T_0 = 1;
 
-    vector<double> q = { K * (1 + T_D / T_0), -K * (1 + 2 * T_D / T_0 - T_0 / T), K * T_D / T_0 };
-    vector<double> e = { 0, 0, 0 };
-    vector<double> y = { 0, 0, 0 };
-    vector<double> u = { 1, 1 };
+    double q_0;
+    double q_1;
+    double q_2;
+
+    vector<double> q;
+    vector<double> e;
+    vector<double> y;
+    vector<double> u;
 
 public:
-    double sum() {
+    func() : q_0(K* (1 + T_D / T_0)), q_1(-K * (1 + 2 * T_D / T_0 - T_0 / T)), q_2(K* T_D / T_0),
+        q({ q_0, q_1, q_2 }), e({ 0, 0, 0 }), y({ 0, 0, 0 }), u({ 1, 1 }) {}
+
+    static double sum(const vector<double>& q, const vector<double>& e) {
         double sum = 0;
         for (int i = 0; i < 3; i++) {
             sum += q[i] * e[i];
@@ -31,7 +38,7 @@ public:
             e[0] = setpoint - y[y.size() - 1];
             e[1] = setpoint - y[y.size() - 2];
             e[2] = setpoint - y[y.size() - 3];
-            u[0] = u[1] + sum();
+            u[0] = u[1] + sum(q, e);
             y.push_back(a * y[y.size() - 1] - b * y[y.size() - 2] * y[y.size() - 2] + c * u[0] + d * sin(u[1]));
             u[1] = u[0];
         }
@@ -45,12 +52,13 @@ public:
 int main() {
     double setpoint;
     ofstream out("output.txt");
-    func f;
 
     if (out.is_open()) {
         cout << "Enter the setpoint: ";
         cin >> setpoint;
-        f.nonlin(100, setpoint, 0.5, 0.3, 0.9, 0.7);
+
+        func f;
+        f.nonlin(100, setpoint);
         vector<double> y_values = f.getY();
 
         for (int i = 0; i < y_values.size(); i++) {
