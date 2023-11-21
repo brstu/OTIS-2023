4a8a6ee647b5dfac5783e7e968772aa10c9dd108
Алина Максимович <145441350+vinberoj24@users.noreply.github.com>
2023-11-21 14:58:15 +0300

    Update main.cpp

--- trunk/ii02411/task_01/src/main.cpp
+++ trunk/ii02411/task_01/src/main.cpp
@@ -1,12 +1,6 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
-
-struct NonlinearParameters {
-    double koef_A;
-    double koef_B;
-    double koef_C;
-    double koef_D;
-};
+#include <array>
 
 // Calculation of linear model
 double CalculateLinear(double y, double koef_A, double koef_B, double u) {
@@ -15,15 +9,15 @@ double CalculateLinear(double y, double koef_A, double koef_B, double u) {
 }
 
 // Calculation of nonlinear model
-double CalculateNonlinear(double y, const NonlinearParameters& params, double YPrev, double u) {
-    y = params.koef_A * y - params.koef_B * pow(YPrev, 2) + params.koef_C * u + params.koef_D * sin(u);
+double CalculateNonlinear(double y, double koef_A, double koef_B, double YPrev, double koef_C, double u, double koef_D, double UPrev) {
+    y = koef_A * y - koef_B * pow(YPrev, 2) + koef_C * u + koef_D * sin(UPrev);
     return y;
 }
 
 int main() {
     const int quantity = 50;
 
-    double linY[quantity];
+    std::array<double, quantity> linY;
     double koef_A = 0.8;
     double koef_B = 1.5;
     double y = 0.0;
@@ -38,16 +32,20 @@ int main() {
 
     // Nonlinear model
     double unLinY[quantity];
-    NonlinearParameters params;
-    params.koef_A = 0.75;
-    params.koef_B = 0.1;
-    params.koef_C = 2.2;
-    params.koef_D = 8.5;
+    koef_A = 0.75;
+    koef_B = 0.1;
+    double koef_C = 2.2;
+    double koef_D = 8.5;
     y = 0.0;
+    u = 0.3;
+    double Ynext = 0.0;
+    double Yprev = 0.0;
 
     for (int i = 0; i < quantity; i++) {
-        double Yprev = y;
-        y = unLinY[i] = CalculateNonlinear(y, params, Yprev, u);
+        Yprev = y;
+        y = Ynext;
+        Ynext = CalculateNonlinear(y, koef_A, koef_B, Yprev, koef_C, u, koef_D, u);
+        unLinY[i] = Ynext;
         std::cout << i + 1 << " " << unLinY[i] << "\n";
     }
 
