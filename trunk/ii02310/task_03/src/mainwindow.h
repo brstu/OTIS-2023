41b35a16acf4d13b884f5e687f91776316306e66
LapinVladimir <vladimir.lapin.91@bk.ru>
2023-11-29 20:59:55 +0300

    add

---
+++ trunk/ii02310/task_03/src/mainwindow.h
@@ -0,0 +1,53 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+#include <Qstring>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+class Vertex;
+class Edge;
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void updateEdges();
+    void updateVertices();
+    void showGraphInfo();
+    void showeulerInfo();
+    int getNumberOfVertices() {
+        return vertices.length();
+    }
+    int getNumberOfEdges() {
+        return edges.length();
+    }
+    QString getEulerCycle();
+    int getVertexIndex(const QString& vertexName) const;
+    private slots:
+    void on_addVertexButton_clicked();
+    void on_addEdgeButton_clicked();
+
+    void on_removeEdgeButton_clicked();
+
+    void on_removeVertexButton_clicked();
+
+    void on_changeVertexButton_clicked();
+
+    void on_addInformationButton_clicked();
+
+    void on_addClearsceneButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    QList<Vertex*> vertices;
+    QList<Edge*> edges;
+};
+#endif // MAINWINDOW_H
