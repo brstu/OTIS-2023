edb207c6d31eabc67f353d29a402537113ab0522
Алешко Ангелина <143990037+Melivu@users.noreply.github.com>
2023-12-03 21:56:33 +0300

    Update OTIS3.py

--- trunk/ii02401/lab_03/src/OTIS3.py
+++ trunk/ii02401/lab_03/src/OTIS3.py
@@ -3,7 +3,6 @@ from tkinter import simpledialog, filedialog, messagebox
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import gzip
 class GraphEditor:
     def __init__(self, master):
         self.master = master
@@ -31,7 +30,6 @@ class GraphEditor:
         file_menu.add_command(label="Save Graph", command=self.save_graph)
         file_menu.add_separator()
         file_menu.add_command(label="Exit", command=self.master.destroy)
-
     def create_toolbar(self):
         toolbar = tk.Frame(self.master)
         toolbar.pack(side=tk.TOP, fill=tk.X)
@@ -51,23 +49,18 @@ class GraphEditor:
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.canvas_frame)
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.canvas.draw()
-
     def create_info_panel(self):
         self.info_text = tk.Text(self.info_frame, height=15, width=30)
         self.info_text.pack()
-
     def new_graph(self):
         self.graph_name = simpledialog.askstring("Graph Name", "Enter the name of the new graph:")
         self.graph_type = simpledialog.askstring("Graph Type", "Enter the type of the new graph (UNORIENT/ORIENT):")
         self.graph.clear()
-    
     # Если граф ориентированный, устанавливаем соответствующий атрибут
         if self.graph_type == "ORIENT":
             self.graph = nx.DiGraph()
-
         self.update_canvas()
         self.update_info_panel("New graph created.")
-
     def open_graph(self):
         file_path = filedialog.askopenfilename(title="Open Graph", filetypes=[("Graph files", "*.graph")])
         if file_path:
@@ -79,7 +72,6 @@ class GraphEditor:
                 self.update_info_panel(f"Graph '{self.graph_name}' opened.")
             except Exception as e:
                 messagebox.showerror("Error", f"Error opening graph: {e}")
-
     def save_graph(self):
         file_path = filedialog.asksaveasfilename(title="Save Graph", defaultextension=".graph", filetypes=[("Graph files", "*.graph")])
         if file_path:
@@ -87,18 +79,14 @@ class GraphEditor:
             self.graph.graph["type"] = self.graph_type
             nx.write_graphml(self.graph, file_path)
             self.update_info_panel(f"Graph '{self.graph_name}' saved to {file_path}.")
-
     def add_node(self):
         node_name = simpledialog.askstring("Add Node", "Enter the name of the new node:")
         if node_name:
-        # Запрос цвета
             node_color = simpledialog.askstring("Node Color", "Enter the color of the node (e.g., 'red'):")
             self.node_colors[node_name] = node_color
-
         # Запрос формы
             node_shape = simpledialog.askstring("Node Shape", "Enter the shape of the node (e.g., 'o' for circle):")
             self.node_shapes[node_name] = node_shape
-
             self.graph.add_node(node_name)
             self.update_canvas()
             self.update_info_panel(f"Node '{node_name}' added with color '{node_color}' and shape '{node_shape}'.")
@@ -120,7 +108,6 @@ class GraphEditor:
                     raise ValueError("Invalid edge description.")
             except Exception as e:
                 messagebox.showerror("Error", f"Error adding edge: {e}")
-
     def delete_node(self):
         node_name = simpledialog.askstring("Delete Node", "Enter the name of the node to delete:")
         if node_name:
@@ -130,7 +117,6 @@ class GraphEditor:
                 self.update_info_panel(f"Node '{node_name}' deleted.")
             else:
                 messagebox.showwarning("Warning", f"Node '{node_name}' not found.")
-
     def delete_edge(self):
         edge_str = simpledialog.askstring("Delete Edge", "Enter the edge description (e.g., 'A -> B'):")
         if edge_str:
@@ -149,7 +135,6 @@ class GraphEditor:
                     raise ValueError("Invalid edge description.")
             except Exception as e:
                 messagebox.showerror("Error", f"Error deleting edge: {e}")
-
     def rename_node(self):
         old_name = simpledialog.askstring("Ren Nod", "Enter the current name of the node:")
         new_name = simpledialog.askstring("Rena Nod", "Enter the new name of the node:")
@@ -160,7 +145,6 @@ class GraphEditor:
                 self.update_info_panel(f"Node '{old_name}' renamed to '{new_name}'.")
             else:
                 messagebox.showwarning("Warning", f"Node '{old_name}' not found.")
-
     def move_node(self):
         node_name = simpledialog.askstring("Mve Nod", "Enter the name of the node to move:")
         new_position = simpledialog.askstring("Mov Nde", "Enter the new position (e.g., '0.5,0.5'):")
@@ -175,7 +159,6 @@ class GraphEditor:
                     messagebox.showwarning("Warning", f"Node '{node_name}' not found.")
             except ValueError:
                 messagebox.showerror("Error", "Invalid position format. Use 'x, y'.")
-
     def show_graph_info(self):
         info_str = f"Graph Name: {self.graph_name}\nGraph Type: {self.graph_type}\n\n"
         info_str += f"Number of Nodes: {len(self.graph.nodes)}\n"
@@ -188,15 +171,12 @@ class GraphEditor:
         info_str += f"Is Tree: {nx.is_tree(self.graph)}\n"
         info_str += f"Is Connected: {nx.is_connected(self.graph)}\n"
         info_str += f"Is Eulerian: {nx.is_eulerian(self.graph)}\n"
-    
     # Добавляем информацию о радиусе, диаметре и центре
         if self.graph:
             info_str += f"Radius: {nx.radius(self.graph)}\n"
             info_str += f"Diameter: {nx.diameter(self.graph)}\n"
             info_str += f"Center: {nx.center(self.graph)}\n"
-    
         self.update_info_panel(info_str)
-
     def find_paths(self):
         src_node = simpledialog.askstring("Fin Path", "Enter the source node:")
         dest_node = simpledialog.askstring("Fnd Pats", "Enter the destination node:")
@@ -208,23 +188,17 @@ class GraphEditor:
                 self.update_info_panel(info_str)
             except Exception as e:
                 messagebox.showerror("Error", f"Error finding paths: {e}")
-
     def update_canvas(self):
         self.ax.clear()
         pos = nx.get_node_attributes(self.graph, 'pos') if 'pos' in nx.get_node_attributes(self.graph, 'pos') else nx.spring_layout(self.graph)
-
         for node, shape in self.node_shapes.items():
             nx.draw_networkx_nodes(self.graph, pos, nodelist=[node], node_size=300, node_color=self.node_colors.get(node, 'blue'), node_shape=shape)
-
         nx.draw_networkx_edges(self.graph, pos, ax=self.ax, connectionstyle='arc3,rad=0.1')
         nx.draw_networkx_labels(self.graph, pos, font_weight='bold', ax=self.ax)
-
         self.canvas.draw()
-
     def update_info_panel(self, info_str):
         self.info_text.delete(1.0, tk.END)
         self.info_text.insert(tk.END, info_str)
-        
     def find_eulerian_cycle(self):
         try:
             eulerian_cycle = list(nx.eulerian_circuit(self.graph))
