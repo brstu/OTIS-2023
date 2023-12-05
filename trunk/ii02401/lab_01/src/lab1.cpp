59fb5c04a2d901e44457f997077261a9e8f52cbb
Алешко Ангелина <143990037+Melivu@users.noreply.github.com>
2023-12-05 20:00:35 +0300

    Update lab1.cpp

--- trunk/ii02401/lab_01/src/lab1.cpp
+++ trunk/ii02401/lab_01/src/lab1.cpp
@@ -14,49 +14,43 @@ struct ModelParameters {
     double u_prev;
     int i;
     int t;
-    const std::vector<double> y_lin;
-    const std::vector<double> y_nonlin;
+    std::vector<double> y_lin;
+    std::vector<double> y_nonlin;
 
-    // Constructor to initialize constant vectors
-    ModelParameters() : y_lin({}), y_nonlin({}) {}
+    // Constructor (no initializer list for y_lin and y_nonlin)
+    ModelParameters() : y_lin(), y_nonlin() {}
 };
 
 // Modify the function to take a struct instead of individual parameters
-void linear_model(const ModelParameters& params) {
+void linear_model(ModelParameters& params) {
     if (params.i <= params.t) {
         std::cout << params.y << std::endl;
-        // Add elements to the vector if needed
-        // params.y_lin.push_back(params.y);
+        params.y_lin.push_back(params.y);
         double y_2 = params.a * params.y + params.b * params.u;
-        ModelParameters new_params = params;
-        new_params.y = y_2;
-        new_params.i += 1;
-        linear_model(new_params);
+        params.y = y_2;
+        params.i += 1;
+        linear_model(params);
     } else {
         std::cout << "END" << std::endl;
     }
 }
 
 // Modify the function to take a struct instead of individual parameters
-void nonlinear_model(const ModelParameters& params) {
+void nonlinear_model(ModelParameters& params) {
     if (params.i == 1) {
         std::cout << params.y << std::endl;
-        // Add elements to the vector if needed
-        // params.y_nonlin.push_back(params.y);
+        params.y_nonlin.push_back(params.y);
         double y_2 = params.a * params.y - params.b * pow(params.y_prev, 2) + params.c * 0 + params.d * sin(0);
-        ModelParameters new_params = params;
-        new_params.y = y_2;
-        new_params.i += 1;
-        nonlinear_model(new_params);
+        params.y = y_2;
+        params.i += 1;
+        nonlinear_model(params);
     } else if (params.i <= params.t) {
         std::cout << params.y << std::endl;
-        // Add elements to the vector if needed
-        // params.y_nonlin.push_back(params.y);
+        params.y_nonlin.push_back(params.y);
         double y_2 = params.a * params.y - params.b * pow(params.y_prev, 2) + params.c * params.u + params.d * sin(params.u_prev);
-        ModelParameters new_params = params;
-        new_params.y = y_2;
-        new_params.i += 1;
-        nonlinear_model(new_params);
+        params.y = y_2;
+        params.i += 1;
+        nonlinear_model(params);
     } else {
         std::cout << "END" << std::endl;
     }
