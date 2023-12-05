6835e0ea0c3fd1c558439415b181c5e914a1922c
Terehov Nikolay <II002420@g.bstu.by>
2023-12-05 12:13:03 +0300

    task1

---
+++ trunk/ii02419/task_01/src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+class Models {
+private:
+    double a = 0.41;
+    double b = 1.4;
+    double c = 0.5;
+    double d = 0.3;
+public:
+    double linearModel(double f, double u) const {
+        double result = a * f + b * u;
+        std::cout << result << std::endl;
+        return result;
+    }
+
+    double nonLinearModel(double f, double prevF, double u) const {
+        double result = a * f - b * prevF * prevF + c * u + d * sin(u);
+        std::cout << result << std::endl;
+        return result;
+    }
+};
+
+int main() {
+    double f = 0.0;
+    double u = 1.0;
+    double n = 100;
+    Models model;
+
+    std::cout << "LinearModeling\n";
+
+    for (int i = 0; i < n; i++) {
+        f = model.linearModel(f, u);
+    }
+
+    std::cout << "---" << std::endl;
+
+    std::cout << "NonLinearModeling\n";
+
+
+    double fn = 0;
+    f = 0;
+    for (int i = 0; i < n; i++) {
+        double prevF = f;
+        f = fn;
+        fn = model.nonLinearModel(f, prevF, u);
+    }
+
+}
