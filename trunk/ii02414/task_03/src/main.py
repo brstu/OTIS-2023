6e3a79e7e6d3434b68e29d1fd871953485e2a9ab
Yury Poddubny <70597955+Yura-108@users.noreply.github.com>
2023-12-08 14:20:15 +0300

    fix bugs 666_4

--- trunk/ii02414/task_03/src/main.py
+++ trunk/ii02414/task_03/src/main.py
@@ -11,68 +11,67 @@ main_label = tk.Label(root, text="Select an action")
 main_label.pack(side=tk.BOTTOM)
 
 # всякие переменные
-click_num = 0
-id_of_edge = 0
-button1 = "<Button-1>"
-num_vertex = "number of vertex"
+click_nums = 0
+id_of_edge_unique = 0
+text_of_button_first = "<Button-1>"
+num_vertexes = "number of vertex"
 id_text_global = "id text"
-text_vertex = "text on vertex"
+text_vertex_global = "text on vertex"
 
-oval_id = None
-ovals = []
-edges = []
+circle_id_num = None
+circle = []
+edges_array = []
 i = 0
 id_text = 0
 tag = 0
 x1, y1, x2, y2 = 0, 0, 0, 0
-cord_edge2 = {'id_vertex1': [], 'id_vertex2': []}
-cord_edge = {'id_edge_text': [], 'id_vertex1': [], 'id_vertex2': []}
-cord = {'id': [], id_text_global: [], text_vertex: [], 'textID': [], num_vertex: [], 'coordinatesX': [],
-        'coordinatesY': []}
-matrix_size = len(cord['id'])
-matrix = [[0] * matrix_size for _ in range(matrix_size)]
+object_edge_first = {'id_vertex1': [], 'id_vertex2': []}
+object_edge_second = {'id_edge_text': [], 'id_vertex1': [], 'id_vertex2': []}
+object_first = {'id': [], id_text_global: [], text_vertex_global: [], 'textID': [], num_vertexes: [], 'coordinatesX': [], 'coordinatesY': []}
+array_size = len(object_first['id'])
+array = [[0] * array_size for _ in range(array_size)]
 tk.Tk.geometry(root, "800x600")
 canvas = tk.Canvas(root, width=1920, height=1080)
 canvas.pack()
-menubar = tk.Menu(root)  # меню
+menubar = tk.Menu(root)
 root.config(menu=menubar)
 
 
 # Function to handle drawing vertices
 def draw_vertex_handler():
     main_label.configure(text="You have chosen to draw vertices, click on the free space to draw the vertex")
-    canvas.unbind(button1)
-    canvas.bind(button1, draw_canvas_handler)
+    canvas.unbind(text_of_button_first)
+    canvas.bind(text_of_button_first, draw_canvas_handler)
 
 
 # Function to handle drawing edges
 def draw_edge_handler():
     main_label.configure(text="You have chosen to draw edges, click on the vertex to draw the edge")
-    canvas.unbind(button1)
-    canvas.bind(button1, draw_canvas_handler_second)
+    canvas.unbind(text_of_button_first)
+    canvas.bind(text_of_button_first, draw_canvas_handler_second)
 
 
 # Function to handle deleting vertices or edges
 def delete_vertex_handler():
     main_label.configure(text="You have chosen to delete a vertex or edge, click on the vertex or edge to delete")
-    canvas.unbind(button1)
-    canvas.bind(button1, delete_canvas_handler)
+    canvas.unbind(text_of_button_first)
+    canvas.bind(text_of_button_first, delete_canvas_handler)
 
 
 # Function to handle renaming vertices
 def rename_vertex_handler():
     main_label.configure(text="You have chosen to rename a vertex, click on the vertex to rename it")
-    canvas.unbind(button1)
-    canvas.bind(button1, rename_handler)
+    canvas.unbind(text_of_button_first)
+    canvas.bind(text_of_button_first, rename_handler)
 
 
 # Function to handle displaying information about vertices and edges
 def edge_click_handler():
     print("Vertex")
-    for key, value in cord.items():
+    for key, value in object_first.items():
         print(key, value)
     print("Edge")
-    for value in cord_edge2.values():
+    for value in object_edge_first.values():
         print(value, end=", ")
 
 
@@ -80,49 +79,49 @@ def edge_click_handler():
 def change_color_handler():
     main_label.configure(
         text="You have chosen to change the color of the vertex, click on the vertex to change its color")
-    canvas.unbind(button1)
-    canvas.bind(button1, colour_handler)
+    canvas.unbind(text_of_button_first)
+    canvas.bind(text_of_button_first, colour_handler)
 
 
 # Function to handle changing the text color of vertices
 def change_text_color_handler():
     main_label.configure(
         text="You have chosen to change the color of the text, click on the vertex to change the color of its text")
-    canvas.unbind(button1)
-    canvas.bind(button1, text_colour_handler)
+    canvas.unbind(text_of_button_first)
+    canvas.bind(text_of_button_first, text_colour_handler)
 
 
 # Function to handle changing the color of edges
 def change_edge_color_handler():
     main_label.configure(text="You have chosen to change the color of the edges, click on the edge to change its color")
-    canvas.unbind(button1)
-    canvas.bind(button1, edge_colour_handler)
+    canvas.unbind(text_of_button_first)
+    canvas.bind(text_of_button_first, edge_colour_handler)
 
 
 # Function to handle deleting vertices or edges based on user click
 def delete_canvas_handler(event):
-    for x in ovals:
+    for x in circle:
         if canvas.find_overlapping(event.x, event.y, event.x, event.y)[0] == x:
             # Delete the vertex
             canvas.delete(x)
-            canvas.delete(cord['textID'][cord['id'].index(x)])
-            ovals.remove(x)
+            canvas.delete(object_first['textID'][object_first['id'].index(x)])
+            circle.remove(x)
 
             # Remove information about the vertex from data structures
-            cord[num_vertex].remove(cord[num_vertex][cord['id'].index(x)])
-            canvas.delete(cord[id_text_global][cord['id'].index(x)])
-            cord['textID'].remove(cord['textID'][cord['id'].index(x)])
-            cord[id_text_global].remove(cord[id_text_global][cord['id'].index(x)])
-            cord["coordinatesX"].remove(cord['coordinatesX'][cord['id'].index(x)])
-            cord["coordinatesY"].remove(cord['coordinatesY'][cord['id'].index(x)])
-            cord[text_vertex].remove(cord[text_vertex][cord['id'].index(x)])
-            cord['id'].remove(x)
+            object_first[num_vertexes].remove(object_first[num_vertexes][object_first['id'].index(x)])
+            canvas.delete(object_first[id_text_global][object_first['id'].index(x)])
+            object_first['textID'].remove(object_first['textID'][object_first['id'].index(x)])
+            object_first[id_text_global].remove(object_first[id_text_global][object_first['id'].index(x)])
+            object_first["coordinatesX"].remove(object_first['coordinatesX'][object_first['id'].index(x)])
+            object_first["coordinatesY"].remove(object_first['coordinatesY'][object_first['id'].index(x)])
+            object_first[text_vertex_global].remove(object_first[text_vertex_global][object_first['id'].index(x)])
+            object_first['id'].remove(x)
             break  # Exit the loop after deleting the first matching vertex
 
 
 # Function to handle drawing vertices
 def draw_canvas_handler(event):
-    global oval_id
+    global circle_id_num
     global i, tag, id_text
     id_text += 1
     i += 1
@@ -132,72 +131,72 @@ def draw_canvas_handler(event):
     texttag = 'text' + str(id_text)
 
     # Store information about the vertex in the data structures
-    cord[num_vertex].append(id_text)
-    cord[id_text_global].append(tag)
-    cord["coordinatesX"].append(x)
-    cord["coordinatesY"].append(y)
+    object_first[num_vertexes].append(id_text)
+    object_first[id_text_global].append(tag)
+    object_first["coordinatesX"].append(x)
+    object_first["coordinatesY"].append(y)
 
     # Create oval (vertex) and text on canvas
-    oval_id = canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill='green', tags=tag)
-    ovals.append(oval_id)
-    cord['id'].append(oval_id)
+    circle_id_num = canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill='green', tags=tag)
+    circle.append(circle_id_num)
+    object_first['id'].append(circle_id_num)
     canvas.create_text(x, y, text=itext, font="Arial 14", tags=texttag)
-    cord['textID'].append(texttag)
-    cord[text_vertex].append(itext)
+    object_first['textID'].append(texttag)
+    object_first[text_vertex_global].append(itext)
 
 
 # Function to handle drawing edges
 def draw_canvas_handler_second(event):
-    global click_num
-    global x1, y1, x2, y2, id_of_edge
-    if click_num == 0:
+    global click_nums
+    global x1, y1, x2, y2, id_of_edge_unique
+    if click_nums == 0:
         # Select the first vertex for the edge
-        for x in ovals:
+        for x in circle:
             if canvas.find_overlapping(event.x, event.y, event.x, event.y)[0] == x:
-                x1 = cord['coordinatesX'][cord['id'].index(x)]
-                y1 = cord['coordinatesY'][cord['id'].index(x)]
-                cord_edge['id_vertex1'].append(x)
-                cord_edge2['id_vertex1'].append(x)
-                click_num = 1
+                x1 = object_first['coordinatesX'][object_first['id'].index(x)]
+                y1 = object_first['coordinatesY'][object_first['id'].index(x)]
+                object_edge_second['id_vertex1'].append(x)
+                object_edge_first['id_vertex1'].append(x)
+                click_nums = 1
                 main_label.configure(text="Select the second vertex to draw the edge")
-    elif click_num == 1:
+    elif click_nums == 1:
         # Select the second vertex and draw the edge
-        for x in ovals:
+        for x in circle:
             if canvas.find_overlapping(event.x, event.y, event.x, event.y)[0] == x:
-                x2 = cord['coordinatesX'][cord['id'].index(x)]
-                y2 = cord['coordinatesY'][cord['id'].index(x)]
-                cord_edge['id_vertex2'].append(x)
-                cord_edge2['id_vertex2'].append(x)
-        tag_edge = 'edge' + str(id_of_edge)
+                x2 = object_first['coordinatesX'][object_first['id'].index(x)]
+                y2 = object_first['coordinatesY'][object_first['id'].index(x)]
+                object_edge_second['id_vertex2'].append(x)
+                object_edge_first['id_vertex2'].append(x)
+        tag_edge = 'edge' + str(id_of_edge_unique)
         line = canvas.create_line(x1, y1, x2, y2, width=4, tags=tag_edge)
-        edges.append(id_of_edge)
-        cord_edge['id_edge_text'].append(tag_edge)
-        id_of_edge += 1
+        edges_array.append(id_of_edge_unique)
+        object_edge_second['id_edge_text'].append(tag_edge)
+        id_of_edge_unique += 1
         canvas.tag_lower(line)
-        click_num = 0
+        click_nums = 0
         main_label.configure(text="Select a vertex to draw an edge on")
 
 
 # Function to handle renaming vertices
 def rename_handler(event):
-    for x in ovals:
+    for x in circle:
         if canvas.find_overlapping(event.x, event.y, event.x, event.y)[0] == x:
             # Prompt user for a new name
             new_name = simpledialog.askstring("Rename", "Enter new name")
-            canvas.delete(cord['textID'][cord['id'].index(x)])
+            canvas.delete(object_first['textID'][object_first['id'].index(x)])
 
             # Create text with the new name on canvas
-            canvas.create_text(cord['coordinatesX'][cord['id'].index(x)], cord['coordinatesY'][cord['id'].index(x)],
-                               text=new_name, font="Arial 13", tags=(cord['textID'][cord['id'].index(x)]))
+            canvas.create_text(object_first['coordinatesX'][object_first['id'].index(x)], object_first['coordinatesY'][object_first['id'].index(x)],
+                               text=new_name, font="Arial 13", tags=(object_first['textID'][object_first['id'].index(x)]))
 
             # Update data structures with the new name
-            cord[text_vertex][cord['id'].index(x)] = new_name
+            object_first[text_vertex_global][object_first['id'].index(x)] = new_name
             break
 
 
 def colour_handler(event):
     # Обработчик события для изменения цвета объекта на холсте
-    for x in ovals:
+    for x in circle:
         if canvas.find_overlapping(event.x, event.y, event.x, event.y)[0] == x:
             new_colour = colorchooser.askcolor()
             color_hex = new_colour[1]
@@ -206,19 +205,19 @@ def colour_handler(event):
 
 def text_colour_handler(event):
     # Обработчик события для изменения цвета текста объекта на холсте
-    for oval in ovals:
+    for oval in circle:
         if canvas.find_overlapping(event.x, event.y, event.x, event.y)[0] == oval:
             # Запрос цвета у пользователя
             new_colour = colorchooser.askcolor()
             color_hex = new_colour[1]
             # Изменение цвета текста
-            canvas.itemconfig(cord['textID'][cord['id'].index(oval)], fill=color_hex)
+            canvas.itemconfig(object_first['textID'][object_first['id'].index(oval)], fill=color_hex)
             break
 
 
 def edge_colour_handler(event):
     # Обработчик события для изменения цвета граней на холсте
-    for edge in edges:
+    for edge in edges_array:
         tag_edge = 'edge' + str(edge)
         if canvas.find_overlapping(event.x, event.y, event.x, event.y)[0] == canvas.find_withtag(tag_edge)[0]:
             # Запрос цвета у пользователя
@@ -231,11 +230,11 @@ def edge_colour_handler(event):
 
 def fill_adjacency_matrix():
     # Заполнение матрицы смежности на основе данных о вершинах и ребрах
-    for edge_index in range(len(cord_edge['id_vertex1'])):
-        vertex1_index = cord['id'].index(cord_edge['id_vertex1'][edge_index])
-        vertex2_index = cord['id'].index(cord_edge['id_vertex2'][edge_index])
-        matrix[vertex1_index][vertex2_index] = 1
-        matrix[vertex2_index][vertex1_index] = 1
+    for edge_index in range(len(object_edge_second['id_vertex1'])):
+        vertex1_index = object_first['id'].index(object_edge_second['id_vertex1'][edge_index])
+        vertex2_index = object_first['id'].index(object_edge_second['id_vertex2'][edge_index])
+        array[vertex1_index][vertex2_index] = 1
+        array[vertex2_index][vertex1_index] = 1
 
 def adjacency_matrix_handler():
     k = 0
@@ -244,7 +243,7 @@ def adjacency_matrix_handler():
     adj_matrix.geometry("250x250")
 
     fill_adjacency_matrix()
-    for i3, val3 in enumerate(matrix):
+    for i3, val3 in enumerate(array):
         adj_matrix_label = tk.Label(adj_matrix, text=str(val3))
         adj_matrix_label.grid(row=k, column=0)
         i3 += 1
@@ -257,12 +256,12 @@ def incidence_matrix_handler():
     inc_matrix.title("Incidence matrix")
     inc_matrix.geometry("250x250")
 
-    for index in range(len(cord_edge['id_vertex1'])):
-        matrix[cord['id'].index(cord_edge['id_vertex1'][index])][
-            edges.index(cord_edge['id_edge_text'].index(cord_edge['id_edge_text'][index]))] = 1
-        matrix[cord['id'].index(cord_edge['id_vertex2'][index])][
-            edges.index(cord_edge['id_edge_text'].index(cord_edge['id_edge_text'][index]))] = 1
-    for index2, value1 in enumerate(matrix):
+    for index in range(len(object_edge_second['id_vertex1'])):
+        array[object_first['id'].index(object_edge_second['id_vertex1'][index])][
+            edges_array.index(object_edge_second['id_edge_text'].index(object_edge_second['id_edge_text'][index]))] = 1
+        array[object_first['id'].index(object_edge_second['id_vertex2'][index])][
+            edges_array.index(object_edge_second['id_edge_text'].index(object_edge_second['id_edge_text'][index]))] = 1
+    for index2, value1 in enumerate(array):
         inc_matrix_label = tk.Label(inc_matrix, text=str(value1))
         inc_matrix_label.grid(row=k, column=0)
         k += 1
@@ -270,34 +269,34 @@ def incidence_matrix_handler():
         print(index2)
 
 def dfs_handler():
-    visited = [False] * len(ovals)
+    visited = [False] * len(circle)
 
     def dfs_rec_handler(vert):
         visited[vert] = True
-        for u, oval in enumerate(ovals):
-            if matrix[vert][u] == 1 and not visited[u]:
+        for u, oval in enumerate(circle):
+            if array[vert][u] == 1 and not visited[u]:
                 dfs_rec_handler(u)
 
     fill_adjacency_matrix()
     count = 0
-    for v in range(len(ovals)):
+    for v in range(len(circle)):
         if not visited[v]:
             dfs_rec_handler(v)
             count += 1
     messagebox.showinfo("DFS", "Graph is connected" if count == 1 else "Graph is not connected")
 
 def bfs_handler():
-    visited = [False] * len(ovals)
+    visited = [False] * len(circle)
 
     def bfs_rec_handler(vert):
         visited[vert] = True
-        for u, oval in enumerate(ovals):
-            if matrix[vert][u] == 1 and not visited[u]:
+        for u, oval in enumerate(circle):
+            if array[vert][u] == 1 and not visited[u]:
                 bfs_rec_handler(u)
 
     fill_adjacency_matrix()
     count = 0
-    for v in range(len(ovals)):
+    for v in range(len(circle)):
         if not visited[v]:
             bfs_rec_handler(v)
             count += 1
