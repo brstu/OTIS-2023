cf7c68f3a82863fda2cee36d9dcf2f954f50bad9
Алешко Ангелина <143990037+Melivu@users.noreply.github.com>
2023-12-10 01:45:18 +0300

    Update lab2.cpp

--- trunk/ii02401/task_02/src/lab2.cpp
+++ trunk/ii02401/task_02/src/lab2.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 #include <cmath>
-#include <cstdlib>
-#include <ctime>
-
-using namespace std;
+#include <random>
+#include <array>
 
 class CustomNonlinearModel {
 public:
-    CustomNonlinearModel(int time, double desiredTemp = 10) : time(time), desiredTemp(desiredTemp) {
-        arrayOfE[0] = 0.003;
-        arrayOfE[1] = 0.15;
-        arrayOfE[2] = 0.0001;
+    CustomNonlinearModel(int time, double desiredTemp = 10) : time(time), desiredTemp(desiredTemp), generator(std::random_device{}()) {
+        arrayOfE = {0.003, 0.15, 0.0001};
 
         // Introduce randomness to the coefficients for a 60% difference
-        arrayOfQ[0] = 0.25 * (1 + 0.6 * ((double)rand() / RAND_MAX - 0.5));
-        arrayOfQ[1] = 0.12 * (1 + 0.6 * ((double)rand() / RAND_MAX - 0.5));
-        arrayOfQ[2] = 0.08 * (1 + 0.6 * ((double)rand() / RAND_MAX - 0.5));
+        arrayOfQ = {0.25 * (1 + 0.6 * getRandom()), 0.12 * (1 + 0.6 * getRandom()), 0.08 * (1 + 0.6 * getRandom())};
 
-        weight[0] = 1.8 * (1 + 0.6 * ((double)rand() / RAND_MAX - 0.5));
-        weight[1] = 0.7 * (1 + 0.6 * ((double)rand() / RAND_MAX - 0.5));
-        weight[2] = 1.8 * (1 + 0.6 * ((double)rand() / RAND_MAX - 0.5));
-        weight[3] = 1.3 * (1 + 0.6 * ((double)rand() / RAND_MAX - 0.5));
+        weight = {1.8 * (1 + 0.6 * getRandom()), 0.7 * (1 + 0.6 * getRandom()), 1.8 * (1 + 0.6 * getRandom()), 1.3 * (1 + 0.6 * getRandom())};
 
         prevU = weight[3];
     }
@@ -42,7 +33,7 @@ public:
             arrayOfE[1] = desiredTemp - futureY;
 
             // Introduce randomness to the calculation of weight[2]
-            weight[2] = prevU + arrayOfQ[0] * arrayOfE[2] + arrayOfQ[1] * arrayOfE[1] + arrayOfQ[2] * arrayOfE[0] * (1 + 0.6 * ((double)rand() / RAND_MAX - 0.5));
+            weight[2] = prevU + arrayOfQ[0] * arrayOfE[2] + arrayOfQ[1] * arrayOfE[1] + arrayOfQ[2] * arrayOfE[0] * (1 + 0.6 * getRandom());
 
             futureY = weight[0] * param[0] + weight[1] * param[1] * param[1] + weight[2] * param[2] + weight[3] * param[3];
             param[1] = param[0];
@@ -54,27 +45,30 @@ public:
         }
     }
 
-    void displayCustomResults() {
-        cout << "Custom Y" << endl;
+    void displayCustomResults() const {
+        std::cout << "Custom Y" << std::endl;
         for (int i = 0; i < time; i++) {
-            cout << result[i] << endl;
+            std::cout << result[i] << std::endl;
         }
     }
 
 private:
     int time;
     double desiredTemp;
-    double arrayOfE[3];
-    double arrayOfQ[3];
-    double weight[4];
+    std::array<double, 3> arrayOfE;
+    std::array<double, 3> arrayOfQ;
+    std::array<double, 4> weight;
     double prevU;
-    double param[4] = {1, 0, 1, 1.0};
-    double result[200];
+    std::array<double, 4> param = {1, 0, 1, 1.0};
+    std::array<double, 200> result;
+    std::mt19937 generator;
+
+    double getRandom() {
+        return ((double)generator() / generator.max()) - 0.5;
+    }
 };
 
 int main() {
-    srand(static_cast<unsigned>(time(0))); // Seed the random number generator
-
     int size = 200;
     CustomNonlinearModel customModel1(size);
     customModel1.performCustomCalculation();
