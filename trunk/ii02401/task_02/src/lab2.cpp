01a39dc54c4a21bbe59fc211385ae5fb8c502702
Алешко Ангелина <143990037+Melivu@users.noreply.github.com>
2023-12-10 14:55:19 +0300

    Update lab2.cpp

--- trunk/ii02401/task_02/src/lab2.cpp
+++ trunk/ii02401/task_02/src/lab2.cpp
@@ -1,89 +1,49 @@
 #include <iostream>
 #include <cmath>
-#include <random>
-#include <array>
 
-class CustomNonlinearModel {
+class functs {
 public:
-    CustomNonlinearModel(int time, double desiredTemp = 10) : time(time), desiredTemp(desiredTemp), generator(getSeededEngine()), arrayOfE{0.003, 0.15, 0.0001} {
-        // Introduce randomness to the coefficients for a 60% difference
-        arrayOfQ = {0.25 * (1 + 0.6 * getRandom()), 0.12 * (1 + 0.6 * getRandom()), 0.08 * (1 + 0.6 * getRandom())};
+    double a = 0.3;
+    double b = 0.4;
+    double c = 0.1;
+    double d = 0.5;
+    double u = 1.0;
+    double y = 0.0;
 
-        weight = {1.8 * (1 + 0.6 * getRandom()), 0.7 * (1 + 0.6 * getRandom()), 1.8 * (1 + 0.6 * getRandom()), 1.3 * (1 + 0.6 * getRandom())};
-
-        prevU = weight[3];
+    void println(double val) {
+        std::cout << val << std::endl;
     }
 
-    void performCustomCalculation() {
-        for (int i = 0; i < time; i++) {
-            param[3] = sin(param[3]);
+    // Нелинейная модель
+    void nonliney(double A, double B, double C, double D, double K, double T0, double TD, double T, double w, double ys) {
+        double q0 = K * (1 + (TD / T0));
+        double q1 = -K * (1 + 2 * (TD / T0) - (T0 / T));
+        double q2 = K * (TD / T0);
+        double u_prev = 1.0;
+        double e_prev1 = w - ys;
+        double e_prev2 = w - ys;
+        double y_prev = ys;
+        double u = 1.0;
 
-            double futureY = weight[0] * param[0] + weight[1] * param[1] * param[1] + weight[2] * param[2] + weight[3] * param[3];
-            param[1] = param[0];
-            param[0] = futureY;
+        std::cout << "Y" << std::endl;
+        while (std::abs(y - w) > 0.1) {
+            double e = w - y;
+            double u = u_prev + q0 * e + q1 * e_prev1 + q2 * e_prev2;
+            y = A * y - B * y_prev + C * u + D * std::sin(u_prev);
+            e_prev2 = e_prev1;
+            e_prev1 = e;
+            u_prev = u;
 
-            arrayOfE[2] = desiredTemp - futureY;
-
-            futureY = weight[0] * param[0] + weight[1] * param[1] * param[1] + weight[2] * param[2] + weight[3] * param[3];
-            param[1] = param[0];
-            param[0] = futureY;
-
-            arrayOfE[1] = desiredTemp - futureY;
-
-            // Introduce randomness to the calculation of weight[2]
-            weight[2] = prevU + arrayOfQ[0] * arrayOfE[2] + arrayOfQ[1] * arrayOfE[1] + arrayOfQ[2] * arrayOfE[0] * (1 + 0.6 * getRandom());
-
-            futureY = weight[0] * param[0] + weight[1] * param[1] * param[1] + weight[2] * param[2] + weight[3] * param[3];
-            param[1] = param[0];
-            param[0] = futureY;
-
-            arrayOfE[0] = desiredTemp - futureY;
-
-            result[i] = param[0];
+            println(y);
         }
-    }
-
-    void displayCustomResults() const {
-        std::cout << "Custom Y" << std::endl;
-        for (int i = 0; i < time; i++) {
-            std::cout << result[i] << std::endl;
-        }
-    }
-
-private:
-    int time;
-    double desiredTemp;
-    std::array<double, 3> arrayOfE;
-    std::array<double, 3> arrayOfQ;
-    std::array<double, 4> weight;
-    double prevU;
-    std::array<double, 4> param{1, 0, 1, 1.0};
-    std::array<double, 200> result;
-    std::mt19937 generator;
-
-    double getRandom() {
-        static std::uniform_real_distribution<double> distribution(-0.5, 0.5);
-        return distribution(generator);
-    }
-
-    std::mt19937 getSeededEngine() {
-        std::random_device rd;
-        std::array<int, std::mt19937::state_size> seed_data;
-        std::generate(seed_data.begin(), seed_data.end(), std::ref(rd));
-        std::seed_seq seq(seed_data.begin(), seed_data.end());
-        return std::mt19937(seq);
     }
 };
 
 int main() {
-    int size = 200;
-    CustomNonlinearModel customModel1(size);
-    customModel1.performCustomCalculation();
-    customModel1.displayCustomResults();
+    functs f;
 
-    CustomNonlinearModel customModel2(size, 15); // Different desired temperature
-    customModel2.performCustomCalculation();
-    customModel2.displayCustomResults();
+    std::cout << "модель" << std::endl;
+    f.nonliney(0.5, 0.6, 0.6, 0.6, 0.8, 1.1, 1, 1.1, 20, 2.0);
 
     return 0;
 }
