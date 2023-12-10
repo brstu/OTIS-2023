ef13e5b742a7dcd23df28f16e3b6bae33e14a5ca
Алешко Ангелина <143990037+Melivu@users.noreply.github.com>
2023-12-10 15:01:05 +0300

    Update lab2.cpp

--- trunk/ii02401/task_02/src/lab2.cpp
+++ trunk/ii02401/task_02/src/lab2.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <cmath>
 
+struct NonlinearParams {
+    double A;
+    double B;
+    double C;
+    double D;
+    double K;
+    double T0;
+    double TD;
+    double T;
+    double w;
+    double ys;
+};
+
 class functs {
 public:
     double a = 0.3;
@@ -10,26 +23,24 @@ public:
     double u = 1.0;
     double y = 0.0;
 
-    void println(double val) {
+    void println(double val) const {
         std::cout << val << std::endl;
     }
 
-    // Нелинейная модель
-    void nonliney(double A, double B, double C, double D, double K, double T0, double TD, double T, double w, double ys) {
-        double q0 = K * (1 + (TD / T0));
-        double q1 = -K * (1 + 2 * (TD / T0) - (T0 / T));
-        double q2 = K * (TD / T0);
+    void nonliney(const NonlinearParams& params) const {
+        double q0 = params.K * (1 + (params.TD / params.T0));
+        double q1 = -params.K * (1 + 2 * (params.TD / params.T0) - (params.T0 / params.T));
+        double q2 = params.K * (params.TD / params.T0);
         double u_prev = 1.0;
-        double e_prev1 = w - ys;
-        double e_prev2 = w - ys;
-        double y_prev = ys;
-        double u = 1.0;
+        double e_prev1 = params.w - params.ys;
+        double e_prev2 = params.w - params.ys;
+        double y_prev = params.ys;
 
         std::cout << "Y" << std::endl;
-        while (std::abs(y - w) > 0.1) {
-            double e = w - y;
+        while (std::abs(y - params.w) > 0.1) {
+            double e = params.w - y;
             double u = u_prev + q0 * e + q1 * e_prev1 + q2 * e_prev2;
-            y = A * y - B * y_prev + C * u + D * std::sin(u_prev);
+            y = params.A * y - params.B * y_prev + params.C * u + params.D * std::sin(u_prev);
             e_prev2 = e_prev1;
             e_prev1 = e;
             u_prev = u;
@@ -41,9 +52,9 @@ public:
 
 int main() {
     functs f;
-
-    std::cout << "модель" << std::endl;
-    f.nonliney(0.5, 0.6, 0.6, 0.6, 0.8, 1.1, 1, 1.1, 20, 2.0);
+    
+    NonlinearParams params = {0.5, 0.6, 0.6, 0.6, 0.8, 1.1, 1, 1.1, 20, 2.0};
+    f.nonliney(params);
 
     return 0;
 }
