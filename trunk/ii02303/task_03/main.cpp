ded31f8c51d7c36e1c1e000622eddb7dfff6a6a7
Vyshinsky Artem Sergeevich <115808321+arciomwww@users.noreply.github.com>
2023-12-12 14:08:51 +0300

    Update main.cpp

--- trunk/ii02303/task_03/main.cpp
+++ trunk/ii02303/task_03/main.cpp
@@ -2,8 +2,11 @@
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
+    //Это строчка запускает программу
     QApplication a(argc, argv);
+    //Создаём окно по имени w
     MainWindow w;
+    //Показываем окно
     w.show();
 
     return a.exec();
