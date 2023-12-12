0481fe7b58f411819c67c7e7aeafa990dde3a2bd
Ежевский Егор <108958075+pinkgrasss@users.noreply.github.com>
2023-12-12 12:20:35 +0300

    Update main.cpp

--- trunk/II02312/task_03/src/main.cpp
+++ trunk/II02312/task_03/src/main.cpp
@@ -4,8 +4,11 @@
 
 int main(int argc, char *argv[])
 {
+    //create app
     QApplication a(argc, argv);
+    //generate window
     MainWindow w;
+    //show window
     w.show();
     return a.exec();
 }
