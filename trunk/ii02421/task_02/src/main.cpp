82d763e62975738926feb1ed1a3e152fc4fb6df1
Черник Владислав <jeffrie.c1o@yandex.ru>
2023-12-12 21:02:34 +0300

    Update main.cpp

--- trunk/ii02421/task_02/src/main.cpp
+++ trunk/ii02421/task_02/src/main.cpp
@@ -5,17 +5,16 @@
 
 class Regulator {
 public:
-    Regulator() : a(0.8), b(0.3), c(0.6), d(0.04), k(0.0002), t(150), td(120), t0(2) {
-    q0 = k * (1 + td / t0);
-    q1 = -k * (1 + 2 * td / t0 - t0 / t);
-    q2 = k * td / t0;
+    Regulator() : a(0.8), b(0.3), c(0.6), d(0.04), k(0.0002), t(150), t0(2), td(120) {
+        q0 = k * (1 + td / t0);
+        q1 = -k * (1 + 2 * td / t0 - t0 / t);
+        q2 = k * td / t0;
 
-    q = {q0, q1, q2};
-    e = {0, 0, 0};
-    y = {0, 0, 0};
-    u = {1, 1};
+        q = {q0, q1, q2};
+        e = {0, 0, 0};
+        y = {0, 0, 0};
+        u = {1, 1};
     }
-
 
     double sum() {
         double result = 0;
@@ -47,7 +46,9 @@ public:
     }
 
 private:
-    double a, b, c, d, k, t, td, t0, q0, q1, q2;
+    double a, b, c, d, k, t, t0;
+    double td = 120;  // In-class initializer for "td"
+    double q0, q1, q2;
     std::vector<double> q, e, y, u;
 };
 
