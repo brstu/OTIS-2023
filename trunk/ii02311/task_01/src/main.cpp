23c213f8854ebc41c4a24a1220e4bcb3f8916366
Kunz Alexandr <shizoyditsme@gmail.com>
2023-12-12 12:41:11 +0300

    [task_01] Add solution problem

--- trunk/ii02311/task_01/src/main.cpp
+++ trunk/ii02311/task_01/src/main.cpp
@@ -1,23 +1,23 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <cmath>
+#include <vector>
 int main()
 {
-	srand(time(NULL));
 	const double a = 0.5;
 	const double b = 0.5;
 	const double c = 0.5;
 	const double d = 0.5;
 	int n=100;
-	double* u = new double[n];
-	double* y = new double[n];
-	y[0] = 0;
-	y[1] = 0;
+	std::vector<double> y; 
+	std::vector<double> u; 
+	
+	y.push_back(0);
 	for (int i = 0; i < n; i++) {
-		u[i] = 1;
+		u.push_back(1);
 	}
 	for (int i = 1; i < n; i++) {
-		y[i] = a * y[i - 1] + b * u[i - 1];
+		y.push_back(a * y[i - 1] + b * u[i - 1]);
 	}
 	std::ofstream out;          
 	out.open("outputs.txt");     
@@ -30,7 +30,7 @@ int main()
 	}
 	out.close();
 	for (int i = 2; i < n; i++) {
-		y[i] = a * y[i - 1] - b * y[i - 2] * y[i - 2] + c * u[i - 1] + d * sin(u[i -2 ]);
+		y.push_back(a * y[i - 1] - b * y[i - 2] * y[i - 2] + c * u[i - 1] + d * sin(u[i -2 ]));
 	}
 	out.open("outputs1.txt");
 	if (out.is_open())
