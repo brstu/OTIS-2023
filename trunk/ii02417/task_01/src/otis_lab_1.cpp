cf67211040d9ef087bf7dc897109beacb1962ed7
Ilya Suprunovich <suprunovichilja@gmail.com>
2023-12-12 12:04:45 +0300

    update

--- trunk/ii02417/task_01/src/otis_lab_1.cpp
+++ trunk/ii02417/task_01/src/otis_lab_1.cpp
@@ -1,47 +1,73 @@
 ﻿#include <iostream>
 #include <cmath>
-#include <array>
-#include <vector>
 
-void LinearModel() {
-    std::cout << "Linear Model" << std::endl;
-    double yT = 1;
-    double uT = 0.5;
-    int time = 15;
-    double a = 1.2;
-    double b = 0.7;
-
-
-    for (int i = 0; i < time; i++) {
-        yT = a * yT + b * uT;
-
-        std::cout << yT << std::endl;
+class LinearModel {
+public:
+    LinearModel() {
+        std::cout << "Linear Model" << std::endl;
+        yT = 1;
+        uT = 0.5;
+        time = 15;
+        a = 1.2;
+        b = 0.7;
     }
-}
 
-void NonLinearModel() {
-    std::cout << "Non-Linear Model" << std::endl;
+    void simulate() {
+        for (int i = 0; i < time; i++) {
+            yT = a * yT + b * uT;
 
-    double yT = 1.5;
-    double uT = 0.5;
-    double prevYT = 0;
-    int time = 15;
-    double a = 1.2;
-    double b = 0.7;
-    double c = -0.32;
-    double d = 0.53;
-
-
-    for (int i = 0; i < time; i++) {
-        yT = a * yT - b * std::pow(prevYT, 2) + c * uT + d * std::sin(uT);
-
-        std::cout << yT << std::endl;
-        prevYT = yT;
+            std::cout << yT << std::endl;
+        }
     }
-}
+
+private:
+    double yT;
+    double uT;
+    int time;
+    double a;
+    double b;
+};
+
+class NonLinearModel {
+public:
+    NonLinearModel() {
+        std::cout << "Non-Linear Model" << std::endl;
+        yT = 1.5;
+        uT = 0.5;
+        prevYT = 0;
+        time = 15;
+        a = 1.2;
+        b = 0.7;
+        c = -0.32;
+        d = 0.53;
+    }
+
+    void simulate() {
+        for (int i = 0; i < time; i++) {
+            yT = a * yT - b * std::pow(prevYT, 2) + c * uT + d * std::sin(uT);
+
+            std::cout << yT << std::endl;
+            prevYT = yT;
+        }
+    }
+
+private:
+    double yT;
+    double uT;
+    double prevYT;
+    int time;
+    double a;
+    double b;
+    double c;
+    double d;
+};
 
 int main() {
-    LinearModel();
-    NonLinearModel();
+    LinearModel linearModel;
+    linearModel.simulate();
+
+    NonLinearModel nonLinearModel;
+    nonLinearModel.simulate();
+
     return 0;
 }
