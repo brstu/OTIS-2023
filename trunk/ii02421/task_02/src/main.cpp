f021f4a199ea9888b246270701071792f4c90d84
Черник Владислав <jeffrie.c1o@yandex.ru>
2023-12-12 20:58:25 +0300

    Update main.cpp

--- trunk/ii02421/task_02/src/main.cpp
+++ trunk/ii02421/task_02/src/main.cpp
@@ -5,26 +5,17 @@
 
 class Regulator {
 public:
-    Regulator() {
-        a = 0.8;
-        b = 0.3;
-        c = 0.6;
-        d = 0.04;
+    Regulator() : a(0.8), b(0.3), c(0.6), d(0.04), k(0.0002), t(150), td(120), t0(2) {
+    q0 = k * (1 + td / t0);
+    q1 = -k * (1 + 2 * td / t0 - t0 / t);
+    q2 = k * td / t0;
 
-        k = 0.0002;
-        t = 150;
-        td = 120;
-        t0 = 2;
-
-        q0 = k * (1 + td / t0);
-        q1 = -k * (1 + 2 * td / t0 - t0 / t);
-        q2 = k * td / t0;
-
-        q = {q0, q1, q2};
-        e = {0, 0, 0};
-        y = {0, 0, 0};
-        u = {1, 1};
+    q = {q0, q1, q2};
+    e = {0, 0, 0};
+    y = {0, 0, 0};
+    u = {1, 1};
     }
+
 
     double sum() {
         double result = 0;
