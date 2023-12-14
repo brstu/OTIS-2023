eb6fc7f2f241d84cc770f82c6e8e1e7232d4c3ae
dlinniy18 <152160227+dlinniy18@users.noreply.github.com>
2023-12-15 00:34:21 +0300

    Update 1 .py

--- trunk/ii02413/task_03/src/1 .py
+++ trunk/ii02413/task_03/src/1 .py
@@ -1,8 +1,8 @@
 from tkinter import messagebox, Canvas, Label, Entry, Button, Tk
 from tkinter.colorchooser import askcolor
-from numpy.random import randint
-from numpy import sqrt
+import numpy as np
 import networkx as nx
+from numpy import sqrt
 
 def line_intersect_circle(x1, y1, x2, y2):
     main_gipotenusa = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -15,8 +15,9 @@ def line_intersect_circle(x1, y1, x2, y2):
 class GraphNode:
     def __init__(self, name):
         self.name = name
-        self.x = randint(0, 636)
-        self.y = randint(0, 596)
+        rng = np.random.default_rng()
+        self.x = rng.integers(0, 636)
+        self.y = rng.integers(0, 596)
 
         self.circle = create_circle(self.x, self.y, 20, fill=color_vertex)
         self.text = canvas.create_text(self.x, self.y, anchor='center', text=name, font="Arial 10", fill="black")
@@ -109,6 +110,127 @@ class GraphEdge:
         canvas.delete(self.text)
         graph.remove_edge(self.node1.name, self.node2.name)
 
+def menu_add_vertex():
+    global color_vertex
+    add_window = Tk()
+    add_window.title("Добавление вершины")
+    add_window.geometry("190x120+1050+250")
+    add_window.wm_attributes('-topmost', 3)
+    add_window.resizable(False, False)
+    label = Label(add_window, text="Введите имя вершины")
+    entry_name = Entry(add_window)
+    add_button = Button(add_window, text="Выбрать цвет", command=lambda: chose_color(color_lable))
+    color_button = Button(add_window, text="Добавить вершину", command=lambda: create_vertex(entry_name, add_window))
+    color_lable = Label(add_window, width=2, bg="white")
+    label.grid(row=0, column=0, sticky="ew")
+    entry_name.grid(row=1, column=0, sticky="ewns")
+    add_button.grid(row=2, column=0, sticky="ewns")
+    color_button.grid(row=3, column=0, sticky="ewns")
+    color_lable.grid(row=1, column=1)
+    add_window.mainloop()
+
+def chose_color(color_lable):
+    global color_vertex
+    rgb, hx = askcolor()
+    print(rgb)
+    color_vertex = hx
+    color_lable.config(bg=color_vertex)
+
+def menu_add_edge():
+    add_window = Tk()
+    add_window.title("Добавление ребра")
+    add_window.geometry("220x220+1050+250")
+    add_window.wm_attributes('-topmost', 3)
+    add_window.resizable(False, False)
+    label1 = Label(add_window, text="Выберите начальную вершину")
+    label2 = Label(add_window, text="Выберите конечную вершину")
+    label3 = Label(add_window, text="Введите вес ребра")
+    entry_weight = Entry(add_window)
+    entry_weight.grid(row=2, column=0, sticky="ewns")
+    label1.grid(row=0, column=0, sticky="ewns")
+    label2.grid(row=0, column=1, sticky="ewns")
+    label3.grid(row=1, column=0, columnspan=2, sticky="ewns")
+    edges_add_window(add_window, 1, 2, 0, 1, entry_weight)
+    add_window.mainloop()
+
+def edges_add_window(add_window, col1, col2, row1, row2, entry_weight):
+    edges_list = list(graph.nodes)
+    entry_weight.grid(row=row2, column=col1, sticky="ewns")
+    start_vertex = Button(add_window, text="Стартовая вершина", command=lambda: edges_list_add(add_window, col1, col2, row1, row2, entry_weight, edges_list))
+    end_vertex = Button(add_window, text="Конечная вершина", command=lambda: edges_list_add(add_window, col1, col2, row1, row2, entry_weight, edges_list))
+    start_vertex.grid(row=row1, column=col1, sticky="ewns")
+    end_vertex.grid(row=row1, column=col2, sticky="ewns")
+
+def edges_list_add(add_window, col1, col2, row1, row2, entry_weight, edges_list):
+    menu = Menu(add_window, tearoff=0)
+    for vertex in edges_list:
+        menu.add_command(label=vertex, command=lambda v=vertex: set_edge(add_window, col1, col2, row1, row2, entry_weight, v))
+    menu.post(10, 10)
+
+def set_edge(add_window, col1, col2, row1, row2, entry_weight, vertex):
+    entry_weight.delete(0, END)
+    entry_weight.insert(0, vertex)
+
+def eulerian_cycle():
+    try:
+        path = list(nx.eulerian_circuit(graph))
+        draw_edge_path(path, "green")
+        root.update()
+        print(path)
+    except nx.NetworkXError:
+        messagebox.showinfo("Ошибка", "Граф не содержит эйлеров цикл")
+
+def shortest_path():
+    start_vertex = simpledialog.askstring("Ввод", "Введите начальную вершину")
+    end_vertex = simpledialog.askstring("Ввод", "Введите конечную вершину")
+    try:
+        path = nx.shortest_path(graph, source=start_vertex, target=end_vertex, weight='weight')
+        draw_edge_path(path, "blue")
+        root.update()
+        print(path)
+    except (nx.NetworkXError, nx.NodeNotFound):
+        messagebox.showinfo("Ошибка", "Путь не найден")
+
+def draw_edge_path(path, color):
+    for i in range(len(path) - 1):
+        for edge in edges:
+            if (edge.node1.name == path[i] and edge.node2.name == path[i + 1]) or \
+               (edge.node1.name == path[i + 1] and edge.node2.name == path[i]):
+                edge.change_color(color)
+                root.after(500)
+                break
+
+def move_node(event):
+    x, y = event.x, event.y
+    for node in nodes:
+        if node.x - 20 < x < node.x + 20 and node.y - 20 < y < node.y + 20:
+            node.move(x, y)
+
+def change_name_or_weight(event):
+    x, y = event.x, event.y
+    for node in nodes:
+        if node.x - 20 < x < node.x + 20 and node.y - 20 < y < node.y + 20:
+            node.change()
+            break
+    for edge in edges:
+        x1, y1, x2, y2 = canvas.coords(edge.line)
+        if x1 - 20 < x < x2 + 20 and y1 - 20 < y < y2 + 20:
+            edge.change()
+            break
+
+def delete(event):
+    x, y = event.x, event.y
+    for node in nodes:
+        if node.x - 20 < x < node.x + 20 and node.y - 20 < y < node.y + 20:
+            node.delete()
+            break
+    for edge in edges:
+        x1, y1, x2, y2 = canvas.coords(edge.line)
+        if x1 - 20 < x < x2 + 20 and y1 - 20 < y < y2 + 20:
+            edge.delete()
+            break
+
+# Создаем окно
 nodes = []  # Список имен вершин
 edges = []  # Список ребер
 color_vertex = "#fff"
