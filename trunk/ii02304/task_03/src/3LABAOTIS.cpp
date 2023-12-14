0aa20d1f8c98a68b4799e73f87b06088d3fc7e5f
Gavrishuk Vladislav Romanovich <vgavrishuk43@gmail.com>
2023-12-14 11:52:38 +0300

    update

--- trunk/ii02304/task_03/src/3LABAOTIS.cpp
+++ trunk/ii02304/task_03/src/3LABAOTIS.cpp
@@ -1,12 +1,8 @@
-﻿// 3LABAOTIS.cpp: определяет точку входа для приложения.
-//
-
-#include "3LABAOTIS.h"
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+﻿#include <cstdlib>
+//открытие лабы
+int main() {
+    std::system("python alg_button_lib.py");
+    std::system("python export.py");
+    std::system("python lib.py");
+    return 0;
 }
