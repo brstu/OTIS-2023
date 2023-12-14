54c53ceff3cfe2f320458d47de62f1cd86033a52
Gavrishuk Vladislav Romanovich <vgavrishuk43@gmail.com>
2023-12-14 11:57:00 +0300

    u

---
+++ trunk/ii02304/task_03/src/3LABAOTIS.cpp
@@ -0,0 +1,8 @@
+﻿#include <cstdlib>
+//открытие лабы
+int main() {
+    std::system("python alg_button_lib.py");
+    std::system("python export.py");
+    std::system("python lib.py");
+    return 0;
+}
