ba4435556e197bba778beb0e712b0d19e72e5205
Привезенцев Михаил Александрович <80481742+NecodevEnjoyer@users.noreply.github.com>
2023-12-14 14:17:48 +0300

    Update main.cpp

--- trunk/ii02311/task_02/src/main.cpp
+++ trunk/ii02311/task_02/src/main.cpp
@@ -1,53 +1,47 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 
-const double a = 0.5;
-const double b = 1;
-const double c = 0.15;
-const double d = 0.3;
-const double u = 1.2;
+using namespace std;
 
-void linearModel(double& y) {
-    y = a * y + b * u;
-    std::cout << y << std::endl;
+const double A = 0.5, B = 0.6, C = 0.6, D = 0.6;
+const double K = 0.8, T0 = 1.1, TD = 1.0, T = 1.1, w = 20;
+
+double calculateQ0() {
+    return K * (1 + (TD / T0));
 }
 
-void nonLinearModel(double& y, double& prevY, bool& secondIteration) {
-    if (secondIteration) {
-        prevY = y;
-        y = a * y + c * u + d * std::sin(u); 
-        std::cout << y << std::endl;
-        secondIteration = false;
-    } else {
-        double temp;
-        temp = a * y - b * std::pow(prevY, 2) + c * u + d * std::sin(u);
-        prevY = y;
-        y = temp;
-        std::cout << y << std::endl;
+double calculateQ1() {
+    return -K * (1 + 2 * (TD / T0) - (T0 / T));
+}
+
+double calculateQ2() {
+    return K * (TD / T0);
+}
+
+void unliner() {
+    const short int y_start = 2;
+    vector<double> y = { y_start, y_start };
+    double u = 1.0;
+    vector<double> e = { w - y_start, w - y_start };
+    vector<double> u_pr = { u, u };
+    
+    double q0 = calculateQ0();
+    double q1 = calculateQ1();
+    double q2 = calculateQ2();
+
+    while (abs(w - y.back()) > 0.01) {
+        e.push_back(w - y.back());
+        u = u_pr.back() + q0 * e.back() + q1 * e[e.size() - 2] + q2 * e[e.size() - 3];
+        y.push_back(A * y.back() - B * y[y.size() - 2] + C * u + D * sin(u_pr.back()));
+        u_pr.push_back(u);
+    }
+
+    cout << "y\te\tu_pr\n";
+    for (int i = 0; i < y.size(); i++) {
+        cout << i + 1 << ";" << y[i] << ";" << e[i] << ";" << u_pr[i] << endl;
     }
 }
 
 int main() {
-    double linearY, nonLinearY, prevY;
-    bool secondIteration = true;
-    
-    std::cout << "Введите температуру: ";
-    std::cin >> linearY;
-    
-    nonLinearY = linearY;
-    int numSteps = 10;
-    
-    std::cout << "Линейная модель" << std::endl;
-    std::cout << linearY << std::endl;
-    for (int i = 0; i < numSteps; i++) {
-        linearModel(linearY);
-    }
-    
-    std::cout << std::endl << "Нелинейная модель" << std::endl;
-    std::cout << nonLinearY << std::endl;
-    for (int i = 0; i < numSteps; i++) {
-        nonLinearModel(nonLinearY, prevY, secondIteration);
-    }
-    
-    return 0;
+    unliner();
 }
