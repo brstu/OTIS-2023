160e5c0bd1aaed02034705d1f49174c7594f764b
Gavrishuk Vladislav Romanovich <vgavrishuk43@gmail.com>
2023-12-14 10:56:05 +0300

    [update]

---
+++ trunk/ii02304/task_03/src/OTISLABA3.cpp
@@ -0,0 +1,8 @@
+﻿#include <cstdlib>
+
+int main() {
+    system("${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/alg_button_lid.py");
+    system("${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/export.py");
+    system("${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/lib.py");
+    return 0;
+}
