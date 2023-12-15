c634fe5e6a07b825e17249d510ffb37169702e1e
Makarevich Kirill Aleksandrovich <112726516+DemiurgeNC@users.noreply.github.com>
2023-12-15 23:51:54 +0300

    Update labb1.cpp

--- trunk/ii02313/task_01/src/labb1.cpp
+++ trunk/ii02313/task_01/src/labb1.cpp
@@ -6,31 +6,31 @@ const double COEFFICIENT_B = 0.02;
 const double COEFFICIENT_C = 1;
 const double COEFFICIENT_D = 1;
 
-// Calculate the temperature using a linear model
-void linod(double y, double j, int tend, std::ofstream& outputFile) {
-    for (int t = 1; t <= tend; ++t) {
+// Calculate the temperature using a linear model recursively
+void linod(double y, double j, int t, int tend) {
+    if (t <= tend) {
         double y_ced = COEFFICIENT_A * y + COEFFICIENT_B * j;
         y = y_ced;
         std::cout << y_ced << "\n";
-        outputFile << y_ced << "\n";
+        linod(y, j, t + 1, tend);
     }
 }
 
-// Calculate the temperature using a nonlinear model
-void nonlinod(double y_brv, double y, double j, int tend, std::ofstream& outputFile) {
-    for (int t = 1; t <= tend; ++t) {
+// Calculate the temperature using a nonlinear model recursively
+void nonlinod(double y_brv, double y, double j, int t, int tend) {
+    if (t <= tend) {
         double y_ced = COEFFICIENT_A * y - COEFFICIENT_B * pow(y_brv, 2) + COEFFICIENT_C * j + COEFFICIENT_D * sin(j);
         y_brv = y;
         y = y_ced;
         std::cout << y_ced << "\n";
-        outputFile << y_ced << "\n";
+        nonlinod(y, y_ced, j, t + 1, tend);
     }
 }
 
 // Ending alert output
 void answer(int Tend, int warm) {
     if (Tend > warm) {
-    std::cout << "\nModeling ended.\n";
+        std::cout << "\nModeling ended.\n";
     }
 }
 
@@ -39,45 +39,40 @@ int main() {
     int warm;
     int Tend;
 
-    //Setting starting temp
+    // Setting starting temp
     std::cout << "Enter starting temperature: ";
     std::cin >> initialTemperature;
 
-    //Checking if starting temp is acceptable
+    // Checking if starting temp is acceptable
     while (initialTemperature < 0 || initialTemperature > 100) {
-    std::cout << "Starting temperature must be from 0 to 100. Please try again: ";
-    std::cin >> initialTemperature;
+        std::cout << "Starting temperature must be from 0 to 100. Please try again: ";
+        std::cin >> initialTemperature;
     }
 
-    //Setting warming
+    // Setting warming
     std::cout << "Enter warming: ";
     std::cin >> warm;
 
-    //Setting end time
+    // Setting end time
     std::cout << "Enter end time: ";
     std::cin >> Tend;
 
-    //Checking if end time is acceptable
+    // Checking if end time is acceptable
     while (Tend < warm) {
-    std::cout << "End time is not tolerable . Please try again: ";
-    std::cin >> Tend;
+        std::cout << "End time is not tolerable. Please try again: ";
+        std::cin >> Tend;
     }
 
-    std::ofstream outputFile("output.txt");
-    
-    //Output of linear modeling
+    // Output of linear modeling
     std::cout << "\nResults of linear modeling:\n";
-    linod(initialTemperature, warm, Tend, outputFile);
+    linod(initialTemperature, warm, 1, Tend);
 
-    //Output of linear modeling
+    // Output of nonlinear modeling
     std::cout << "\nResult of nonlinear modeling:\n";
-    nonlinod(0, initialTemperature, warm, Tend, outputFile);
+    nonlinod(0, initialTemperature, warm, 1, Tend);
 
-    // Close the output file
-    outputFile.close();
-    
     // Finishing task
     answer(Tend, warm);
-    
+
     return 0;
 }
