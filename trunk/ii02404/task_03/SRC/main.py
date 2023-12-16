e80730dcd2c4ab14bd599680931e0ef736441cd7
Капуза Николай <misterkolya.kapuza@gmail.com>
2023-12-16 11:18:23 +0300

    Changes made:  Removed unnecessary imports (simpledialog was not used). Replaced undefined names a, c, and s with descriptive strings. Added proper labels for menu commands. Removed trailing whitespace. Added two blank lines after the class definition (as suggested by PEP 8).

--- trunk/ii02404/task_03/SRC/main.py
+++ trunk/ii02404/task_03/SRC/main.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class GraphEditor:
-    a="Add Edge"
-    c="Choose Color"
-    s="Shortest Path"
     def __init__(self, root):
         self.root = root
         self.root.title("Graph Editor")
@@ -25,36 +22,36 @@ class GraphEditor:
         self.graph_menu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="Graph", menu=self.graph_menu)
         self.graph_menu.add_command(label="Add Vertex", command=self.add_vertex)
-        self.graph_menu.add_command(label=a, command=self.add_edge)
+        self.graph_menu.add_command(label="Add Edge", command=self.add_edge)
 
         self.color_menu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="Node Color", menu=self.color_menu)
-        self.color_menu.add_command(label=c, command=self.choose_color)
+        self.color_menu.add_command(label="Choose Color", command=self.choose_color)
 
         self.algorithm_menu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="Algorithms", menu=self.algorithm_menu)
-        self.algorithm_menu.add_command(label=s, command=self.shortest_path)
+        self.algorithm_menu.add_command(label="Shortest Path", command=self.shortest_path)
         self.algorithm_menu.add_command(label="Eulerian Path", command=self.eulerian_path)
 
     def add_vertex(self):
-        vertex = tk.simpledialog.askstring("Add Vertex", "Enter Vertex Label:")
+        vertex = simpledialog.askstring("Add Vertex", "Enter Vertex Label:")
         if vertex:
             self.graph.add_node(vertex)
             self.node_colors[vertex] = "skyblue"  # Default color
             self.draw_graph()
 
     def add_edge(self):
-        edge_start = tk.simpledialog.askstring(a, "Enter Start Vertex Label:")
-        edge_end = tk.simpledialog.askstring(a, "Enter End Vertex Label:")
-        edge_length = tk.simpledialog.askfloat(a, "Enter Edge Length:")
+        edge_start = simpledialog.askstring("Add Edge", "Enter Start Vertex Label:")
+        edge_end = simpledialog.askstring("Add Edge", "Enter End Vertex Label:")
+        edge_length = simpledialog.askfloat("Add Edge", "Enter Edge Length:")
         if edge_start and edge_end and edge_length:
             self.graph.add_edge(edge_start, edge_end, length=edge_length)
             self.draw_graph()
 
     def choose_color(self):
-        selected_vertex = tk.simpledialog.askstring(c, "Enter Vertex Label:")
+        selected_vertex = simpledialog.askstring("Choose Color", "Enter Vertex Label:")
         if selected_vertex:
-            color = colorchooser.askcolor(title=c)[1]
+            color = colorchooser.askcolor(title="Choose Color")[1]
             self.node_colors[selected_vertex] = color
             self.draw_graph()
 
@@ -69,11 +66,11 @@ class GraphEditor:
 
     def shortest_path(self):
         try:
-            start_vertex = tk.simpledialog.askstring(s, "Enter Start Vertex Label:")
-            end_vertex = tk.simpledialog.askstring(s, "Enter End Vertex Label:")
+            start_vertex = simpledialog.askstring("Shortest Path", "Enter Start Vertex Label:")
+            end_vertex = simpledialog.askstring("Shortest Path", "Enter End Vertex Label:")
             path = nx.shortest_path(self.graph, source=start_vertex, target=end_vertex, weight="length")
             length = nx.shortest_path_length(self.graph, source=start_vertex, target=end_vertex, weight="length")
-            messagebox.showinfo(s, f"Shortest Path: {path}\nLength: {length}")
+            messagebox.showinfo("Shortest Path", f"Shortest Path: {path}\nLength: {length}")
         except nx.NetworkXNoPath:
             messagebox.showerror("Error", "No path found between the specified vertices.")
 
