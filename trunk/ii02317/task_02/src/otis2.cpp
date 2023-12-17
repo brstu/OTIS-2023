3c58f7ccac09918a7466595c690e72f7502fda6a
novittskaya <novitskayavera03@gmail.com>
2023-12-17 21:13:12 +0300

    x

--- trunk/ii02317/task_02/src/otis2.cpp
+++ trunk/ii02317/task_02/src/otis2.cpp
@@ -17,28 +17,28 @@ private:
     double q2 = (K * Td) / h;
     
     vector<double> q = {q0, q1, q2};
-    vector<double> e = {0, 0, 0};
+    vector<double> E = {0, 0, 0};
     vector<double> y = {0, 0, 0};
-    vector<double> u = {1, 1};
+    vector<double> U = {1, 1};
 public:
     double sum() {
         double sum = 0;
         for (int i = 0; i < 3; i++) {
-            sum += q[i] * e[i];
+            sum += q[i] * E[i];
         }
         return sum;
     }
-    void nline(int time, double setpoint, double a = 0.5, double b = 0.3, double c = 0.9, double d = 0.7) {
-        for (int i = 0; i < time; i++) {
-            e[0] = setpoint - y[y.size() - 1]; //count e0
-            e[1] = setpoint - y[y.size() - 2]; //count e1
-            e[2] = setpoint - y[y.size() - 3]; //count e2
-            u[0] = u[1] + sum(); //count u
-            y.push_back(a * y[y.size() - 1] - b * y[y.size() - 2] * y[y.size() - 2] + c * u[0] + d * sin(u[1]));
-            u[1] = u[0];
+    void nline(int Time, double Point, double a = 0.5, double b = 0.3, double c = 0.9, double d = 0.7) {
+        for (int i = 0; i < Time; i++) {
+            E[0] = Point - y[y.size() - 1]; //count E0
+            E[1] = Point - y[y.size() - 2]; //count E1
+            E[2] = Point - y[y.size() - 3]; //count E2
+            U[0] = U[1] + sum(); //count U
+            y.push_back(a * y[y.size() - 1] - b * y[y.size() - 2] * y[y.size() - 2] + c * U[0] + d * sin(U[1]));
+            U[1] = U[0];
         }
     }
-    vector<double> getY() const{ 
+    vector<double> getY() const{
         return y;
     }
 };
@@ -64,3 +64,5 @@ int main() {
     }
     return 0;
 }
+
+
