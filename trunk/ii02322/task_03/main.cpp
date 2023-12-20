cf1bcb0c9cdd0d4d25d8dc24bf464779fb219d8a
Tutina Elizaveta Dmitrievna <145442986+Eliza0756@users.noreply.github.com>
2023-12-20 19:04:15 +0300

    Update main.cpp

--- trunk/ii02322/task_03/main.cpp
+++ trunk/ii02322/task_03/main.cpp
@@ -1,10 +1,22 @@
+// Include the main window header file
 #include "mainwindow.h"
+
+// Include the QApplication header file
 #include <QApplication>
 
+// Define the main function
 int main(int argc, char *argv[]) {
 
+    // Create an instance of QApplication
     QApplication a(argc, argv);
+
+    // Create an instance of MainWindow
     MainWindow w;
+
+    // Show the MainWindow instance
     w.show();
+
+    // Execute the application
     return QApplication::exec();
 }
+
