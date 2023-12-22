0c3206a545ac805918a1e7cff77151cd47cb6bc2
Швороб Владислав Андреевич <136325247+Ampersand04@users.noreply.github.com>
2023-12-22 17:03:18 +0300

    fix

--- trunk/II02323/task_03/src/main.py
+++ trunk/II02323/task_03/src/main.py
@@ -22,20 +22,20 @@ def calculate_circle_intersection(x1, y1, x2, y2):
     return x2 - dx, y2 - dy, x1 + dx, y1 + dy
 class GraphNode:
     def __init__(self, name):
+        seed_value = 42
+        np_random.seed(seed_value)
         self.name = name
-        self.radius = radius
-        self.color = color
         self.x = np_random.default_rng().integers(0, 636)
         self.y = np_random.default_rng().integers(0, 596)
 
-        self.circle = create_circle(self.x, self.y, 20, self.radius, fill=color_vertex)
+        self.circle = create_circle(self.x, self.y, 20, fill=color_vertex)
         self.text = canvas.create_text(self.x, self.y, anchor='center', text=name, font="Arial 10", fill="black")
         graph.add_node(name)
     def move(self, x, y):
         self.x = x
         self.y = y
         canvas.coords(self.circle, x-20, y-20, x+20, y+20)
-        canvas.coords(self.text, x, y)
+        canvas.coords(self.text, x, y)  
         for edge in edges:
             if edge.node1 == self or edge.node2 == self:
                 edge.move()
@@ -72,13 +72,21 @@ class GraphNode:
 def create_circle(x, y, r, **kwargs):
     return canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 class Edge:
+    LINE_WIDTH = 2
+    LINE_COLOR = "black"
+    FONT_SIZE = 20
+    TEXT_COLOR = "white"
+
     def __init__(self, node1, node2, weight: int):
         self.weight = weight
         self.node1 = node1
         self.node2 = node2
-        self.line = canvas.create_line(calculate_circle_intersection(self.node1.x, self.node1.y, self.node2.x, self.node2.y),width=2, fill="black")
-        self.text = canvas.create_text((node1.x + node2.x) / 2, (node1.y + node2.y) / 2 - 5, anchor='center',text=self.weight, font="Arial 20", fill="white")
+        self.line = canvas.create_line(self.calculate_line_coordinates(), width=self.LINE_WIDTH, fill=self.LINE_COLOR)
+        self.text = canvas.create_text((node1.x + node2.x) / 2, (node1.y + node2.y) / 2 - 5,
+                                       anchor='center', text=self.weight, font=f"Arial {self.FONT_SIZE}",
+                                       fill=self.TEXT_COLOR)
         graph.add_edge(node1.name, node2.name, weight=weight)
+
     def change(self):
         win = Tk()
         win.title("Изменение веса")
@@ -94,10 +102,14 @@ class Edge:
         button.place(x=10, y=70)
         win.mainloop()
     def change_weight(self, weight):
-        self.weight = int(weight)
-        canvas.itemconfig(self.text, text=weight)
-        graph.remove_edge(self.node1.name, self.node2.name)
-        graph.add_edge(self.node1.name, self.node2.name, weight=self.weight)
+        try:
+            new_weight = int(weight)
+            self.weight = new_weight
+            canvas.itemconfig(self.text, text=new_weight)
+            graph.remove_edge(self.node1.name, self.node2.name)
+            graph.add_edge(self.node1.name, self.node2.name, weight=new_weight)
+        except ValueError:
+            print("Invalid input. Please enter a valid integer for the weight.")
     def color_change_function(self, color):
         canvas.itemconfig(self.line, fill=color)
     def move(self):
@@ -107,13 +119,15 @@ class Edge:
         canvas.delete(self.line)
         canvas.delete(self.text)
         graph.remove_edge(self.node1.name, self.node2.name)
+    def calculate_line_coordinates(self):
+        return calculate_circle_intersection(self.node1.x, self.node1.y, self.node2.x, self.node2.y)
 #рисуем вершину
 def create_vertex(entry_name, window):
     name = entry_name.get()
     vertexs.append(GraphNode(name))
     window.destroy()
 #выбор цвета
-def chose_color(color_lable):
+def choose_color(color_lable):
     global color_vertex
     rgb, hx= askcolor()
     print(rgb)
@@ -138,7 +152,7 @@ def menu_add_vertex():
 }
     entry_name = Entry(add_window, **entry_style)
     entry_name.grid(padx=10, pady=10)
-    add_button = Button(add_window, text = "Задать цвет",command = lambda: chose_color(color_lable))
+    add_button = Button(add_window, text = "Задать цвет",command = lambda: choose_color(color_lable))
     color_button = Button(add_window, text = "Добавить вершину",command = lambda: create_vertex(entry_name,add_window))
     color_lable = Label(add_window,width=2,bg = "white")
     label.grid(row=0, column=0, sticky="ew")
@@ -147,12 +161,12 @@ def menu_add_vertex():
     color_button.grid(row=3, column=0, sticky="ewns", padx=10, pady=10)
     color_lable.grid(row = 1,column=1, padx=10, pady=10)
     add_window.mainloop()
-# создание ребра
-def create_edge(entry_weight, entry_node1, entry_node2, window):
+# Function to create an edge
+def create_edge(entry_weight, entry_node1, entry_node2, edge_window):
     try:
         weight = int(entry_weight.get())
     except ValueError:
-        messagebox.showerror("Ошибка", "Вес ребра должен быть числом")
+        messagebox.showerror("Error", "Edge weight should be a number")
     else:
         node1 = entry_node1.get()
         node2 = entry_node2.get()
@@ -162,14 +176,18 @@ def create_edge(entry_weight, entry_node1, entry_node2, window):
             if vertex.name == node2:
                 node2 = vertex
         edges.append(Edge(node1, node2, weight))
-        window.destroy()
+        edge_window.destroy()
 #меню добавления ребер
 def menu_add_edge():
+    """
+    Display a window for adding an edge to the graph.
+    """
     add_window = Tk()
-    add_window.title("Добавление ребра")
+    add_window.title("Add Edge")
     add_window.geometry("200x230+1050+250")
     add_window.wm_attributes('-topmost', 3)
     add_window.resizable(False, False)
+
     entry_style = {
         'font': ('Arial', 9),
         'width': 25,
@@ -190,29 +208,30 @@ def menu_add_edge():
         'font': ('Arial', 9),
     }
 
-    # Элементы интерфейса для ребра
-    label = Label(add_window, text="Укажите вес ребра", **label_style)
+    # Elements for edge input
+    label_weight = Label(add_window, text="Укажите вес ребра", **label_style)
     entry_weight = Entry(add_window, **entry_style)
-    label2 = Label(add_window, text="Первая вершина", **label_style)
+    label_node1 = Label(add_window, text="Первая вершина", **label_style)
     entry_node1 = Entry(add_window, **entry_style)
-    label3 = Label(add_window, text="Вторая вершина", **label_style)
+    label_node2 = Label(add_window, text="Вторая вершина", **label_style)
     entry_node2 = Entry(add_window, **entry_style)
-    add_button = Button(add_window, text="Задать цвет", command=lambda: chose_color(color_label), **button_style)
-    color_button = Button(add_window, text="Добавить ребро", command=lambda: create_edge(entry_weight, entry_node1, entry_node2, add_window), **button_style)
+    color_button = Button(add_window, text="Задать цвет", command=lambda: choose_color(color_label), **button_style)
+    add_button = Button(add_window, text="Добавить ребро", command=lambda: create_edge(entry_weight, entry_node1, entry_node2, add_window), **button_style)
     color_label = Label(add_window, width=2, bg="white", **label_style)
 
-    # Размещение элементов в окне
-    label.grid(row=0, column=0, sticky="ew")
+    # Placement of elements in the window
+    label_weight.grid(row=0, column=0, sticky="ew")
     entry_weight.grid(row=1, column=0, sticky="ewns", padx=10, pady=5)
-    label2.grid(row=2, column=0, sticky="ew")
+    label_node1.grid(row=2, column=0, sticky="ew")
     entry_node1.grid(row=3, column=0, sticky="ewns", padx=10, pady=5)
-    label3.grid(row=4, column=0, sticky="ew")
+    label_node2.grid(row=4, column=0, sticky="ew")
     entry_node2.grid(row=5, column=0, sticky="ewns", padx=10, pady=5)
-    add_button.grid(row=6, column=0, sticky="ewns", padx=10, pady=5)
-    color_button.grid(row=7, column=0, sticky="ewns", padx=10, pady=5)
+    color_button.grid(row=6, column=0, sticky="ewns", padx=10, pady=5)
+    add_button.grid(row=7, column=0, sticky="ewns", padx=10, pady=5)
     color_label.grid(row=1, column=1, padx=10, pady=5)
 
     add_window.mainloop()
+
 def node_movement_function(event):
     for node in vertexs:
         if node.x - 25 < event.x < node.x + 25 and node.y - 25 < event.y < node.y + 25:
